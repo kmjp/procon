97b85eee47452ec5235121eb35a773f9f717c9eb
kmjp <kmjp@users.noreply.github.com>
2015-02-23 23:03:04 +0900

    Refine CF#290 Div1 B.

--- codeforce/281-300/cf290/b.cpp
+++ codeforce/281-300/cf290/b.cpp
@@ -13,30 +13,26 @@ typedef signed long long ll;
 
 int N;
 ll L[1000],C[1000];
-ll mi=1LL<<60;
-
-map<ll,ll> M[305];
+map<ll,ll> M;
 
 void solve() {
 	int i,j,k,l,r,x,y; string s;
 	
 	cin>>N;
 	FOR(i,N) cin>>L[i];
-	FOR(i,N) cin>>C[i];
 	FOR(i,N) {
-		M[i+1][L[i]]=C[i];
-		ITR(it,M[i]) {
+		cin>>C[i];
+		if(M.count(L[i])) M[L[i]]=min(M[L[i]],C[i]);
+		else M[L[i]]=C[i];
+		ITR(it,M) {
 			ll X=__gcd(it->first,L[i]);
-			if(M[i+1].count(X)==0) M[i+1][X]=it->second+C[i];
-			else M[i+1][X]=min(M[i+1][X],it->second+C[i]);
-			if(M[i+1].count(it->first)==0) M[i+1][it->first]=it->second;
-			else M[i+1][it->first]=min(M[i+1][it->first],it->second);
+			if(M.count(X)) M[X]=min(M[X],it->second+C[i]);
+			else M[X]=it->second+C[i];
 		}
-		M[i].clear();
-		if(M[i+1].count(1)) mi=min(mi,M[i+1][1]);
 	}
 	
-	if(mi>=1LL<<60) mi=-1;
+	ll mi=-1;
+	if(M.count(1)) mi=M[1];
 	cout<<mi<<endl;
 }
 
