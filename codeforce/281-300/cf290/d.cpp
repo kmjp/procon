49fb28379b8d847e7926b8f140222218a2637aa6
kmjp <kmjp@users.noreply.github.com>
2015-02-26 01:53:14 +0900

    Add CF#290 Div1D.

--- codeforce/281-300/cf290/d.cpp
+++ codeforce/281-300/cf290/d.cpp
@@ -11,130 +11,97 @@ typedef signed long long ll;
 #define MINUS(a) memset(a,0xff,sizeof(a))
 //-------------------------------------------------------
 
-int N,M;
-int mat[101][101];
-int NG[101],num;
 ll mo=1000000009;
-vecor<int> E[101];
-int NN[101];
-int vis[101];
-int vis2[101][101];
-ll dp[101][101][101];
+int N,M;
+int mat[101][101],mat2[101][101];
+int in[101], gr[101], gr2[101], root[101];
+int NG[101];
+ll dp[101][101],tdp[102];
 
-static const int N_=1020;
-static ll C_[N_][N_];
+const int CN=201;
+ll C[CN][CN];
 
-class SCC_BI {
-public:
-	static const int MV = 5000;
-	vector<int> E[MV];
-	stack<int> roots,S;
-	int NV,ord[MV],llink[MV],inin[MV],time;
-	vector<int> ART; // out
-	vector<vector<int> > SC; // out
-	vector<pair<int,int> > BR; // out
-	
-	SCC_BI(int NV=MV) { this->NV=NV; for(int i=0;i<MV;i++) E[i].clear();}
-	void add_edge(int x,int y) { E[x].push_back(y); E[y].push_back(x); }
-	void dfs(int cur,int pre) {
-		int art=0,conn=0,i;
-		ord[cur]=llink[cur]=++time;
-		S.push(cur); inin[cur]=1; roots.push(cur);
-		FOR(i,E[cur].size()) {
-			int tar=E[cur][i];
-			if(ord[tar]==0) {
-				conn++; dfs(tar,cur);
-				llink[cur]=min(llink[cur],llink[tar]);
-				art += (pre!=-1 && ord[cur]<=llink[tar]);
-				if(ord[cur]<llink[tar]) BR.push_back(make_pair(min(cur,tar),max(cur,tar)));
-			}
-			else if(tar!=pre) {
-				llink[cur]=min(llink[cur],ord[tar]);
-				while(inin[tar]&&ord[roots.top()]>ord[tar]) roots.pop();
-			}
-		}
-		
-		if(cur==roots.top()) {
-			SC.push_back(vector<int>());
-			while(1) {
-				i=S.top(); S.pop(); inin[i]=0;
-				SC.back().push_back(i);
-				if(i==cur) break;
-			}
-			sort(SC.back().begin(),SC.back().end());
-			roots.pop();
-		}
-		if(art || (pre==-1&&conn>1)) ART.push_back(cur);
+ll dfs(int cur,int pre,int inloop=0) {
+	int num=0,i,j,k,x;
+	ZERO(dp[cur]);
+	dp[cur][0]=1;
+	FOR(x,N) if(mat[cur][x] && x!=pre) {
+		int tn=dfs(x,cur);
+		for(i=num;i>=0;i--) for(j=tn;j>=1;j--) (dp[cur][i+j] += dp[cur][i]*dp[x][j]%mo*C[i+j][j]) %= mo;
+		num += tn;
 	}
-	void scc() {
-		SC.clear(),BR.clear(),ART.clear();
-		ZERO(ord);ZERO(llink);ZERO(inin);time=0;
-		for(int i=0;i<NV;i++) if(!ord[i]) dfs(i,-1);
-		sort(BR.begin(),BR.end()); sort(ART.begin(),ART.end());
-	}
-};
-
-int dfs(int cur,int pre) {
-	int ret=1,i;
-	FOR(i,E[cur].size()) if(pre!=E[cur][i]) ret+=dfs(E[cur][i],cur);
-	return ret;
+	num++;
+	if(inloop==0) dp[cur][num]=dp[cur][num-1];
+	return num;
 }
 
-void dfs2(int cur,int pre) {
-	int ret=1,i;
-	if(vis[pre][cur]>=0) return;
-	vis[pre][cur] = 1;
-	ll dp2[2][102];
-	int c=0,t=1,x,y;
-	
-	dp2[0][1]=1;
-	FOR(i,E[cur].size()) {
-		int tar=E[cur][i];
-		if(tar==pre) continue;
-		dfs(tar,cur);
-		ZERO(dp2[t]);
-		FOR(x,vis[pre][cur]+1) {
-			FOR(y,vis[cur][tar]+1) {
-				dp2[t][x+y]+=dp2[c][x]*dp[cur][tar][y]%mo*C_[x+y][y]%mo;
-			}
-		}
-		vis[pre][cur]+=vis[cur][tar];
-		FOR(x,vis[pre][cur]+1) dp2[t][x] %= mo;
-		swap(c,t);
-	}
-	FOR(i,101) dp[pre][cur][i]=dp2[c][i];
-	return ret;
+ll modpow(ll a, ll n = mo-2) {
+	ll r=1;
+	while(n) r=r*((n%2)?a:1)%mo,a=a*a%mo,n>>=1;
+	return r;
 }
+
 
 void solve() {
 	int i,j,k,l,r,x,y; string s;
 	
-	FOR(i,N_) C_[i][0]=C_[i][i]=1;
-	for(i=1;i<N_;i++) for(j=1;j<i;j++) C_[i][j]=(C_[i-1][j-1]+C_[i-1][j])%mo;
+	FOR(i,CN) for(j=0;j<=i;j++) C[i][j]=(j==0||j==i)?1:(C[i-1][j-1]+C[i-1][j])%mo;
 	
 	cin>>N>>M;
-	SCC_BI sb(N);
 	FOR(i,M) {
 		cin>>x>>y;
-		mat[x-1][y-1]=mat[y-1][x-1]=1;
-		sb.add_edge(x-1,y-1);
-	}
-	sb.scc();
-	num=N;
-	FOR(i,sb.SC.size()) {
-		if(sb.SC[i].size()>1) FOR(j,sb.SC[i].size()) NG[sb.SC[i][j]]=1, num--;
-	}
-	FOR(x,N) FOR(y,N) if(NG[x]||NG[y]) mat[x][y]=0;
-	FOR(x,N) FOR(y,N) if(mat[x][y]) E[x].push_back(y);
-	FOR(i,N) FOR(x,N) FOR(y,N) mat[x][y]|=mat[x][i]&&mat[i][y];
-	
-	MINUS(vis2);
-	FOR(i,N) if(NG[i]==0) {
-		NN[i]=dfs(i,-1);
-		FOR(x,N) if(mat[i][x]) NN[x]=NN[i];
-		FOR(x,N) if(mat[i][x]) dfs2(x,100);
+		mat[x-1][y-1]=mat[y-1][x-1]=mat2[x-1][y-1]=mat2[y-1][x-1]=1;
+		in[x-1]++;
+		in[y-1]++;
 	}
 	
+	queue<int> q;
+	FOR(i,N) NG[i]=1;
+	FOR(i,N) if(in[i]<=1) q.push(i);
+	
+	while(q.size()) {
+		int k=q.front();
+		q.pop();
+		if(NG[k]==0) continue;
+		NG[k]=0;
+		FOR(i,N) if(mat[k][i] && NG[i]==1 && --in[i]<=1) q.push(i);
+	}
+	
+	FOR(i,N) FOR(x,N) FOR(y,N) mat2[x][y] |= mat2[x][i]&&mat2[i][y];
+	
+	FOR(i,N) {
+		gr[i]=gr2[i]=i;
+		// union find
+		FOR(j,N) if(mat2[j][i]) gr[i]=min(gr[i],j);
+		// compact cycle
+		FOR(j,N) if(mat2[j][i] && NG[i]&&NG[j]) gr2[i]=min(gr2[i],j);
+		if(gr2[i]!=i) FOR(j,N) if(mat[i][j]) mat[gr2[i]][j] |= 1, mat[j][gr2[i]] |= 1, mat[i][j]=mat[j][i]=0;
+	}
+	FOR(i,N) mat[i][i]=0, root[i]=-1;
+	FOR(i,N) if(NG[i]) root[gr[i]]=gr2[i];
+	
+	tdp[0]=1;
+	
+	FOR(i,N) if(gr[i]==i) {
+		ll tdp2[102]={}, tdp3[102]={};
+		if(root[i]==-1) {
+			FOR(x,N) if(gr[x]==i) {
+				y=dfs(x,-1);
+				FOR(j,y+1) (tdp2[j]+=dp[x][j])%=mo;
+			}
+			
+			FOR(j,y+1) tdp2[j]=tdp2[j]*modpow(max(1,y-j))%mo;
+		}
+		else {
+			y=dfs(root[i],-1,1);
+			FOR(x,y+1) tdp2[x]=dp[root[i]][x];
+		}
+		FOR(j,y+1) FOR(x,N+1) if(x+j<=N) (tdp3[x+j]+=tdp[x]*tdp2[j]%mo*C[x+j][j])%=mo;
+		FOR(j,N+1) tdp[j]=tdp3[j];
+		
+	}
+	
+	FOR(i,N+1) cout<<tdp[i]<<endl;
 	
 }
 
