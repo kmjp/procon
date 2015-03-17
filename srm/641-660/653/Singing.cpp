77174ab4e1df2c2aa2a1765a792576e45fa2f0c6
kmjp <kmjp@users.noreply.github.com>
2015-03-17 23:50:31 +0900

    Refine SRM653 Div1 Medium.

--- srm/641-660/653/Singing.cpp
+++ srm/641-660/653/Singing.cpp
@@ -10,28 +10,40 @@ typedef signed long long ll;
 #define MINUS(a) memset(a,0xff,sizeof(a))
 //-------------------------------------------------------
 
-template<class V> class MaxFlow_Ford {
+template<class V> class MaxFlow_dinic {
 public:
 	struct edge { int to,reve;V cap;};
-	static const int MV = 10000;
+	static const int MV = 2200;
 	vector<edge> E[MV];
-	int vis[MV];
+	int itr[MV],lev[MV];
 	
 	void add_edge(int x,int y,V cap,bool undir=false) {
 		E[x].push_back((edge){y,(int)E[y].size(),cap});
 		E[y].push_back((edge){x,(int)E[x].size()-1,undir?cap:0});
 	}
 	
+	void bfs(int cur) {
+		MINUS(lev);
+		queue<int> q;
+		lev[cur]=0;
+		q.push(cur);
+		while(q.size()) {
+			int v=q.front(); q.pop();
+			ITR(e,E[v]) if(e->cap>0 && lev[e->to]<0) lev[e->to]=lev[v]+1, q.push(e->to);
+		}
+	}
+	
 	V dfs(int from,int to,V cf) {
-		int i; V tf;
 		if(from==to) return cf;
-		vis[from]=1;
-		FOR(i,E[from].size()) {
-			edge& e=E[from][i];
-			if(vis[e.to]==0 && e.cap>0 && (tf = dfs(e.to,to,min(cf,e.cap)))>0) {
-				e.cap -= tf;
-				E[e.to][e.reve].cap += tf;
-				return tf;
+		for(;itr[from]<E[from].size();itr[from]++) {
+			edge* e=&E[from][itr[from]];
+			if(e->cap>0 && lev[from]<lev[e->to]) {
+				V f=dfs(e->to,to,min(cf,e->cap));
+				if(f>0) {
+					e->cap-=f;
+					E[e->to][e->reve].cap += f;
+					return f;
+				}
 			}
 		}
 		return 0;
@@ -40,9 +52,10 @@ public:
 	V maxflow(int from, int to) {
 		V fl=0,tf;
 		while(1) {
-			ZERO(vis);
-			if((tf = dfs(from,to,numeric_limits<V>::max()))==0) return fl;
-			fl+=tf;
+			bfs(from);
+			if(lev[to]<0) return fl;
+			ZERO(itr);
+			while((tf=dfs(from,to,numeric_limits<V>::max()))>0) fl+=tf;
 		}
 	}
 };
@@ -52,7 +65,7 @@ class Singing {
 	public:
 	int solve(int N, int low, int high, vector <int> pitch) {
 		int i;
-		MaxFlow_Ford<int> mf;
+		MaxFlow_dinic<int> mf;
 		for(i=1;i<=low-1;i++) mf.add_edge(0,i,pitch.size()-1);
 		for(i=high+1;i<=N;i++) mf.add_edge(i,N+1,pitch.size()-1);
 		FOR(i,pitch.size()-1) if(pitch[i]!=pitch[i+1]) mf.add_edge(pitch[i],pitch[i+1],1,true);
@@ -70,8 +83,8 @@ class Singing {
 	void test_case_2() { int Arg0 = 6; int Arg1 = 2; int Arg2 = 5; int Arr3[] = {5,3,1,6,4,2}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 1; verify_case(2, Arg4, solve(Arg0, Arg1, Arg2, Arg3)); }
 	void test_case_3() { int Arg0 = 10; int Arg1 = 4; int Arg2 = 5; int Arr3[] = {1,4,3,5,2,5,7,5,9}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 3; verify_case(3, Arg4, solve(Arg0, Arg1, Arg2, Arg3)); }
 	void test_case_4() { int Arg0 = 100; int Arg1 = 20; int Arg2 = 80; int Arr3[] = {2,27,3,53,53,52,52,60,85,89,100,53,60,2,3,53,100,89,40,42,2,53,2,85}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); int Arg4 = 5; verify_case(4, Arg4, solve(Arg0, Arg1, Arg2, Arg3)); }
-
 // END CUT HERE
+
 
 
 };
