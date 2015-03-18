e37c195aa48f629839561a100d576c93c6016498
kmjp <kmjp@users.noreply.github.com>
2015-03-18 21:24:04 +0900

    Add Indeed Now Qualification Round B A/B/C/D.

---
+++ 2015/indeed/qual_b/c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<to;x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N;
+vector<int> E[101000];
+set<int> S;
+vector<int> R;
+int vis[101000];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N-1) {
+		cin>>x>>y;
+		E[x-1].push_back(y-1);
+		E[y-1].push_back(x-1);
+	}
+	S.insert(0);
+	while(S.size()) {
+		x=*S.begin();
+		S.erase(S.begin());
+		if(vis[x]) continue;
+		
+		R.push_back(x+1);
+		vis[x]=1;
+		FOR(i,E[x].size()) S.insert(E[x][i]);
+	}
+	
+	FOR(i,N) _P("%d%s",R[i], (i==N-1)?"\n":" ");
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
