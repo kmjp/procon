d4bf0cddac3c6138b76ed43ed206259293b252ad
kmjp <kmjp@users.noreply.github.com>
2015-04-02 03:15:45 +0900

    Refine UTPC 2014 L.

--- 2015/utpc2014/l.cpp
+++ 2015/utpc2014/l.cpp
@@ -12,32 +12,15 @@ typedef signed long long ll;
 #define MINUS(a) memset(a,0xff,sizeof(a))
 //-------------------------------------------------------
 
-class UF {
-	public:
-	int um;
-	vector<int> ufpar,ufrank,ufcnt;
-	UF(int um_=100002) { um=um_; ufrank=vector<int>(um,0); ufcnt=vector<int>(um,1); for(int i=0;i<um;i++) ufpar.push_back(i);}
-	int operator[](int x) {return (ufpar[x]==x)?(x):(ufpar[x] = operator[](ufpar[x]));}
-	int count(int x) { return ufcnt[operator[](x)];}
-	void unite(int x,int y) {
-		x = operator[](x); y = operator[](y);
-		if(x==y) return;
-		if(ufrank[x]<ufrank[y]) ufpar[x]=y, ufcnt[y]+=ufcnt[x];
-		else {ufpar[y]=x; ufcnt[x]+=ufcnt[y]; if(ufrank[x]==ufrank[y]) ufrank[x]++;}
-	}
-};
-
-UF uf;
-int num[40];
 vector<int> E[40];
 ll mask[40];
+int mat[40][40];
 
 string S;
-set<char> M;
-map<int,int> R;
 
 ll getcov(int cur,ll cmask,vector<int>& V) {
 	if(cur==V.size()) return cmask;
+	
 	ll ret=getcov(cur+1,cmask | (1LL<<V[cur]),V);
 	
 	FORR(r,E[V[cur]]) if(r<V[cur] && ((cmask&(1LL<<r))==0)) return ret;
@@ -58,16 +41,15 @@ ll pat(vector<int>& V) {
 		else NC.push_back(r);
 	}
 	
+	vector<pair<int,int> >NE;
+	FOR(i,C.size()) FORR(r,E[C[i]]) if(r<C[i] && (cov&(1LL<<r))) NE.emplace_back(C[i],r);
+	
 	FOR(mask,1<<C.size()) {
 		int tot=0;
 		ll p=1;
 		FOR(i,C.size()) val[C[i]]=(mask&(1<<i))!=0;
-		FOR(i,C.size()) {
-			FORR(r,E[C[i]]) if(r<C[i] && (cov&(1LL<<r))) {
-				if(val[C[i]]==val[r]) tot--;
-				else tot++;
-			}
-		}
+		FORR(r,NE) tot += (val[r.first]!=val[r.second])?1:-1;
+		
 		FOR(i,NC.size()) {
 			int n[2]={};
 			FORR(r,E[NC[i]]) n[val[r]]++;
@@ -92,18 +74,17 @@ void solve() {
 		if(S[i]!=S[i+2]) {
 			E[S[i]].push_back(S[i+2]);
 			E[S[i+2]].push_back(S[i]);
-			mask[S[i]] |= 1LL<<S[i+2];
-			mask[S[i+2]] |= 1LL<<S[i];
 		}
-		num[S[i]]++;
-		num[S[i+2]]++;
-		uf.unite(S[i],S[i+2]);
+		mask[S[i]] |= 1LL<<S[i+2];
+		mask[S[i+2]] |= 1LL<<S[i];
+		mat[S[i]][S[i+2]]=mat[S[i+2]][S[i]]=1;
 	}
+	FOR(i,30) FOR(x,30) FOR(y,30) mat[x][y] |= mat[x][i]&mat[i][y];
 	
 	ll ret=1;
-	FOR(i,30) if(uf[i]==i && num[i]) {
+	FOR(i,30) if(mask[i] && count(&mat[i][0],&mat[i][i],1)==0) {
 		vector<int> V;
-		FOR(x,30) if(uf[x]==i) V.push_back(x);
+		FOR(x,30) if(mat[i][x]) V.push_back(x);
 		ret *= pat(V);
 	}
 	cout<<ret<<endl;
