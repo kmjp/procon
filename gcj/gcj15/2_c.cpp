ed282f595515e92291c36521d075aa303565dbcb
kmjp <kmjp@users.noreply.github.com>
2015-06-01 02:10:04 +0900

    Add Google Code Jam 2015 Round2 C.

---
+++ gcj/gcj15/2_c.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+#include <sys/time.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<to;x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N;
+vector<int> P[1010];
+map<string,int> M;
+int mat[4040][4040];
+int type[4040];
+
+template<class V> class MaxFlow_dinic {
+public:
+	struct edge { int to,reve;V cap;};
+	static const int MV = 8100;
+	vector<edge> E[MV];
+	int itr[MV],lev[MV];
+	void add_edge(int x,int y,V cap,bool undir=false) {
+		E[x].push_back((edge){y,(int)E[y].size(),cap});
+		E[y].push_back((edge){x,(int)E[x].size()-1,undir?cap:0});
+	}
+	void bfs(int cur) {
+		MINUS(lev);
+		queue<int> q;
+		lev[cur]=0;
+		q.push(cur);
+		while(q.size()) {
+			int v=q.front(); q.pop();
+			ITR(e,E[v]) if(e->cap>0 && lev[e->to]<0) lev[e->to]=lev[v]+1, q.push(e->to);
+		}
+	}
+	V dfs(int from,int to,V cf) {
+		if(from==to) return cf;
+		for(;itr[from]<E[from].size();itr[from]++) {
+			edge* e=&E[from][itr[from]];
+			if(e->cap>0 && lev[from]<lev[e->to]) {
+				V f=dfs(e->to,to,min(cf,e->cap));
+				if(f>0) {
+					e->cap-=f;
+					E[e->to][e->reve].cap += f;
+					return f;
+				}
+			}
+		}
+		return 0;
+	}
+	V maxflow(int from, int to) {
+		V fl=0,tf;
+		while(1) {
+			bfs(from);
+			if(lev[to]<0) return fl;
+			ZERO(itr);
+			while((tf=dfs(from,to,numeric_limits<V>::max()))>0) fl+=tf;
+		}
+	}
+};
+
+vector<string> split(const string str, char delim){
+	vector<string> res;
+	size_t current = 0, found;
+	while((found = str.find_first_of(delim, current)) != string::npos){
+		res.push_back(string(str, current, found - current));
+		current = found + 1;
+	}
+	res.push_back(string(str, current, str.size() - current));
+	return res;
+}
+
+void solve(int _loop) {
+	int f,i,j,k,l,x,y;
+	string s;
+	
+	cin>>N;
+	getline(cin,s);
+	M.clear();
+	ZERO(mat);
+	FOR(i,4020) mat[i][i]=1;
+	
+	FOR(i,N) {
+		getline(cin,s);
+		vector<string> VS=split(s,' ');
+		
+		P[i].clear();
+		FORR(r,VS) {
+			if(M.count(r)==0) M[r]=M.size()-1;
+			P[i].push_back(M[r]);
+		}
+	}
+	
+	int W=M.size();
+	MaxFlow_dinic<int> mf;
+	
+	mf.add_edge(0,2,101000);
+	mf.add_edge(3,1,101000);
+	FOR(i,W) mf.add_edge(2+N+i,2+N+W+i,1);
+	FOR(i,N) FORR(r,P[i]) mf.add_edge(2+i,2+N+r,101000), mf.add_edge(2+N+W+r,2+i,101000);
+	
+	_P("Case #%d: %d\n",_loop,mf.maxflow(0,1));
+}
+
+void init() {
+}
+
+int main(int argc,char** argv){
+	int loop,loops;
+	long long span;
+	char tmpline[1000];
+	struct timeval start,end,ts;
+	
+	if(argc>1) freopen(argv[1], "r", stdin);
+	gettimeofday(&ts,NULL);
+	cin>>loops;
+	init();
+	
+	for(loop=1;loop<=loops;loop++) {
+		gettimeofday(&start,NULL); solve(loop); gettimeofday(&end,NULL);
+		span = (end.tv_sec - start.tv_sec)*1000000LL + (end.tv_usec - start.tv_usec);
+		fprintf(stderr,"Case : %d                                     time: %lld ms\n",loop,span/1000);
+	}
+	
+	start = ts;
+	span = (end.tv_sec - start.tv_sec)*1000000LL + (end.tv_usec - start.tv_usec);
+	fprintf(stderr,"**Total time: %lld ms\n",span/1000);
+	
+	return 0;
+}
+
+
