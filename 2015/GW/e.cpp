4ab947a4ba806a62b44150c21c94a72b6e87046b
kmjp <kmjp@users.noreply.github.com>
2015-09-10 01:37:48 +0900

    Add GW Contest 2015 E.

---
+++ 2015/GW/e.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<to;x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+ll N,K,L;
+ll mo=1000000007;
+const int prime_max = 1000005;
+int NP,prime[100000],divp[prime_max];
+
+ll modpow(ll a, ll n = mo-2) {
+	ll r=1;
+	while(n) r=r*((n%2)?a:1)%mo,a=a*a%mo,n>>=1;
+	return r;
+}
+
+void cprime() {
+	for(int i=2;i<prime_max;i++) if(divp[i]==0) {
+		//M[i]=NP;
+		prime[NP++]=i;
+		for(ll j=1LL*i*i;j>i&&j<prime_max;j+=i) divp[j]=i;
+	}
+}
+
+vector<ll> enumdiv(ll n) {
+	vector<ll> S;
+	for(ll i=1;i*i<=n;i++) if(n%i==0) {S.push_back(i); if(i*i!=n) S.push_back(n/i); }
+	sort(S.begin(),S.end());
+	return S;
+}
+
+ll combi(ll N_, ll C_) {
+	const int NUM_=2000001;
+	static ll fact[NUM_+1],factr[NUM_+1],inv[NUM_+1];
+	if (fact[0]==0) {
+		inv[1]=fact[0]=factr[0]=1;
+		for (int i=2;i<=NUM_;++i) inv[i] = inv[mo % i] * (mo - mo / i) % mo;
+		for (int i=1;i<=NUM_;++i) fact[i]=fact[i-1]*i%mo, factr[i]=factr[i-1]*inv[i]%mo;
+	}
+	if(C_<0 || C_>N_) return 0;
+	return factr[C_]*fact[N_]%mo*factr[N_-C_]%mo;
+}
+ll hcomb(int P_,int Q_) { return (P_==0&&Q_==0)?1:combi(P_+Q_-1,Q_);}
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cprime();
+	cin>>N>>K>>L;
+	
+	if(L==N) {
+		auto divs=enumdiv(L);
+		ll ret=0;
+		FORR(r,divs) {
+			int phi=r;
+			FOR(i,NP) {
+				if(prime[i]>r) break;
+				if(r%prime[i]==0) phi = phi/prime[i]*(prime[i]-1);
+			}
+			(ret += phi * modpow(K,N/r))%=mo;
+		}
+		cout<< ret*modpow(N) % mo << endl;
+	}
+	else if(L%2==0) {
+		cout<<hcomb(K,N)<<endl;
+	}
+	else {
+		cout<<(hcomb(K,N)+combi(K,N))%mo<<endl;
+	}
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
