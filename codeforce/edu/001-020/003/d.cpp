111e148afadcdb37488ae9d9d741ea5ba09acaaa
kmjp <kmjp@users.noreply.github.com>
2015-12-25 01:04:51 +0900

    Refine ECR#003 D.

--- codeforce/edu/001-020/003/d.cpp
+++ codeforce/edu/001-020/003/d.cpp
@@ -13,12 +13,32 @@ typedef signed long long ll;
 //-------------------------------------------------------
 
 int N,M,K,S;
-int A[202020], B[202020], T[202020], C[202020];
+int A[202020], B[202020], T[202020], C[202020], RA[1000100], RB[1000100];
 vector<pair<int,int> > PP[2];
 ll sum[2][202020];
 
 ll num[202020];
 
+int ok(int d,int out) {
+	int j,x,y;
+	
+	
+	FOR(j,PP[0].size()+1) {
+		int x=K-j;
+		if(x<0) break;
+		if(x>PP[1].size()) continue;
+		if(sum[0][j]*A[d]+sum[1][x]*B[d]<=S) {
+			if(out) {
+				_P("%d\n",d+1);
+				FOR(y,j) _P("%d %d\n",PP[0][y].second,RA[A[d]]+1);
+				FOR(y,x) _P("%d %d\n",PP[1][y].second,RB[B[d]]+1);
+				exit(0);
+			}
+			return 1;
+		}
+	}
+	return 0;
+}
 
 void solve() {
 	int i,j,k,l,r,x,y; string s;
@@ -26,6 +46,12 @@ void solve() {
 	cin>>N>>M>>K>>S;
 	FOR(i,N) cin>>A[i];
 	FOR(i,N) cin>>B[i];
+	FOR(i,N) if(i) {
+		if(A[i]<A[i-1]) RA[A[i]]=i;
+		else A[i]=A[i-1];
+		if(B[i]<B[i-1]) RB[B[i]]=i;
+		else B[i]=B[i-1];
+	}
 	FOR(i,M) {
 		cin>>T[i]>>C[i];
 		T[i]--;
@@ -37,28 +63,10 @@ void solve() {
 	MINUS(num);
 	FOR(i,2) FOR(j,PP[i].size()) sum[i][j+1]=sum[i][j]+PP[i][j].first;
 	
-	int DD=-1,PD=-1;
-	ll D=1LL<<30,P=1LL<<30;
-	FOR(i,N) {
-		if(A[i]<D) D=A[i], DD=i;
-		if(B[i]<P) P=B[i], PD=i;
-		if(DD==i || PD==i) {
-			FOR(j,PP[0].size()+1) {
-				if(sum[0][j]*D>S) break;
-				ll left=S-sum[0][j]*D;
-				ll hoge=left/P;
-				x=(lower_bound(sum[1],sum[1]+(PP[1].size()+1),hoge+1)-sum[1])-1;
-				if(j+x>=K) {
-					_P("%d\n",i+1);
-					FOR(y,j)   _P("%d %d\n",PP[0][y].second,DD+1);
-					FOR(y,K-j) _P("%d %d\n",PP[1][y].second,PD+1);
-					return;
-				}
-				
-			}
-		}
-	}
-	_P("-1\n");
+	if(ok(N-1,0)==0) return _P("-1\n");
+	int d=N-1;
+	for(i=20;i>=0;i--) if(d-(1<<i)>=0 && ok(d-(1<<i),0)) d-=1<<i;
+	ok(d,1);
 	
 }
 
