79632afce779405d5c17f6c6cced2ed17219d781
kmjp <kmjp@users.noreply.github.com>
2016-02-19 03:06:36 +0900

    Add VolBIT Formulas Blitz A-R.

---
+++ 2016/VolBIT/i.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<to;x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+void solve() {
+	ll N,ret=0;
+	
+	cin>>N;
+	//left,right
+	ret=2*3*(1LL<<(2*(2*N-2-N-1)));
+	//other
+	ret+=(N-3)*3*3*(1LL<<(2*(2*N-2-N-2)));
+	cout<<4*ret<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
