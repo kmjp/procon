8487c165e19beae9ac655c2e22540fee86c6f809
kmjp <kmjp@users.noreply.github.com>
2016-04-02 03:33:37 +0900

    Add lay_contest round 00003 A/B.

---
+++ 2016/lay/00003/b.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int T;
+ll N,L,R;
+
+ll mo=1000000009;
+ll combi(ll N_, ll C_) {
+	const int NUM_=420001;
+	static ll fact[NUM_+1],factr[NUM_+1],inv[NUM_+1];
+	if (fact[0]==0) {
+		inv[1]=fact[0]=factr[0]=1;
+		for (int i=2;i<=NUM_;++i) inv[i] = inv[mo % i] * (mo - mo / i) % mo;
+		for (int i=1;i<=NUM_;++i) fact[i]=fact[i-1]*i%mo, factr[i]=factr[i-1]*inv[i]%mo;
+	}
+	if(C_<0 || C_>N_) return 0;
+	return factr[C_]*fact[N_]%mo*factr[N_-C_]%mo;
+}
+ll hcomb(int P_,int Q_) { return (P_==0&&Q_==0)?1:combi(P_+Q_-1,Q_);}
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>T;
+	while(T--) {
+		cin>>N>>L>>R;
+		cout<<(hcomb(N+1,R)-hcomb(N+1,L-1)+mo)%mo<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
