f1091e0ca9979293240aaf2f1b1b9a412b12aa72
kmjp <kmjp@users.noreply.github.com>
2017-01-29 17:48:24 +0900

    Add NJPC 2017 A/B/C/D/E/F/G/H.

---
+++ 2017/njpc/f.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N;
+int A[202020];
+ll dp[5020][5020];
+
+ll ma=-1LL<<60;
+int le=202020;
+int cnt=0;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) cin>>A[i];
+	FOR(x,N) {
+		ll pre[50]={};
+		ll sum=0;
+		ll inc=0;
+		for(y=x;y<N;y++) {
+			sum+=A[y];
+			if(A[y]>0) {
+				FOR(i,A[y]) pre[A[y]]=max(pre[A[y]],pre[i]+A[y]);
+				inc=max(inc,pre[A[y]]);
+			}
+			dp[x][y]=sum-inc;
+			if(dp[x][y]>ma) ma=dp[x][y], le=y-x, cnt=0;
+			if(dp[x][y]==ma) {
+				if(y-x<le) le=y-x, cnt=0;
+				if(y-x==le) cnt++;
+			}
+		}
+	}
+	cout<<ma<<" "<<cnt<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
