8e4e583f432f1098c137dd57aa139b62e3f538e4
kmjp <kmjp@users.noreply.github.com>
2017-06-03 01:43:11 +0900

    Add yukicoder No.521/522/523/524.

---
+++ yuki/501-600/523.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N;
+ll mo=1000000007;
+
+ll rev(ll a) {
+	ll r=1, n=mo-2;
+	while(n) r=r*((n%2)?a:1)%mo,a=a*a%mo,n>>=1;
+	return r;
+}
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	ll a=1,b=1;
+	cin>>N;
+	
+	for(i=1;i<=2*N;i++) b=b*i%mo;
+	for(i=1;i<=N;i++) a=a*2%mo;
+	
+	cout<<b*rev(a)%mo<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
