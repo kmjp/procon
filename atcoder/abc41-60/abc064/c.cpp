6d915df8ceba8e88e05a2e03f300b4d7b6a8414d
kmjp <kmjp@users.noreply.github.com>
2017-06-10 22:59:21 +0900

    Add ABC#064 A/B/C/D.

---
+++ atcoder/abc41-60/abc064/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+
+int N;
+int A[1010];
+int O;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) {
+		cin>>x;
+		if(x<3200) A[x/400]++;
+		else O++;
+	}
+	
+	int count=0;
+	FOR(i,8) if(A[i]>0) count++;
+	
+	cout<<max(1,count)<<" "<<(count+O)<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
