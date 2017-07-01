833a01a5e7f6681ed6215abaacd81bdf5f787a79
kmjp <kmjp@users.noreply.github.com>
2017-07-01 23:46:11 +0900

    Add 2014 JAG Summer Day4 A.

---
+++ 2014/jag/summer_day4/a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int A,B,C;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>A>>B>>C;
+	FOR(i,3600) {
+		int L=(A+B)*i;
+		int R=(A+B)*i+A;
+		
+		if(L%60<=R%60) {
+			if(L%60<=C%60 && C%60<=R%60) {
+				cout<<L/60*60+C%60<<endl;
+				return;
+			}
+		}
+		else {
+			if(L%60<=C%60) {
+				cout<<L/60*60+C%60<<endl;
+				return;
+			}
+			else if(C%60<=R%60) {
+				cout<<R/60*60+C%60<<endl;
+				return;
+			}
+		}
+	}
+	cout<<-1<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
