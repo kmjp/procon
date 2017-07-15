0ec859000d594bb9fe30969299b88a84ff218115
kmjp <kmjp@users.noreply.github.com>
2017-07-15 12:22:05 +0900

    Add 2007 JOI Spring E.

---
+++ 2007/joi/spring/e.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N,P;
+ll mo;
+ll dp[10101];
+
+ll modpow(ll a, ll n = mo-2) {
+	ll r=1;
+	while(n) r=r*((n%2)?a:1)%mo,a=a*a%mo,n>>=1;
+	return r;
+}
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>P>>N;
+	mo=P;
+	FOR(i,P) {
+		x=1;
+		dp[modpow(i,N)]++;
+	}
+	ll ret=0;
+	FOR(i,P) FOR(j,P) {
+		x=i+j;
+		if(x>=P) x-=P;
+		ret += dp[i]*dp[j]*dp[x];
+	}
+	cout<<ret<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n';
+	FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	solve(); return 0;
+}
