8475964005713e2e80c29683daddd07ef1348ad1
kmjp <kmjp@users.noreply.github.com>
2017-10-15 01:25:59 +0900

    Add ABC#075 A/B/C/D.

---
+++ atcoder/abc41-60/abc075/b.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int H,W;
+string S[50];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>H>>W;
+	FOR(y,H) cin>>S[y];
+	FOR(y,H) FOR(x,W) if(S[y][x]=='.') {
+		int num=0;
+		for(int y2=max(0,y-1);y2<=min(H-1,y+1);y2++) {
+			for(int x2=max(0,x-1);x2<=min(W-1,x+1);x2++) {
+				if(S[y2][x2]=='#') num++;
+			}
+		}
+		S[y][x]='0'+num;
+	}
+	FOR(y,H) cout<<S[y]<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
