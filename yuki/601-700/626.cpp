5790a6bdaa6ea7d729db19231d3aa89f1083106e
kmjp <kmjp@users.noreply.github.com>
2018-01-16 02:48:34 +0900

    Fix yukicoder No.626.

--- yuki/601-700/626.cpp
+++ yuki/601-700/626.cpp
@@ -16,7 +16,6 @@ int N;
 ll W;
 pair<ll,ll> P[5050];
 ll ma=0;
-pair<ll,ll> Q[2][150505];
 
 bool cmp(pair<ll,ll> L,pair<ll,ll> R) {
 	return L.first*1.0/L.second>R.first*1.0/R.second;
@@ -26,40 +25,29 @@ void solve() {
 	int i,j,k,l,r,x,y; string s;
 	
 	cin>>N>>W;
-	FOR(i,N) cin>>P[i].first>>P[i].second;
+	FOR(i,N) {
+		cin>>P[i].first>>P[i].second;
+	}
 	sort(P,P+N,cmp);
 	
 	vector<pair<ll,ll>> V;
 	V.push_back({0,0});
-	
 	FOR(i,N) {
-		x=y=0;
-		FORR(e,V) {
-			Q[0][x++]=e;
-			if(e.second+P[i].second<=W) {
-				Q[1][y++]={e.first+P[i].first,e.second+P[i].second};
-			}
-		}
+		ll v=P[i].first,w=P[i].second;
+		vector<pair<ll,ll>> V2=V;
+		FORR(vv,V) if(vv.second+w<=W) V2.push_back({vv.first+v,vv.second+w});
 		V.clear();
-		int x2=0,y2=0;
-		while(V.size()<70000) {
-			if(x2==x&&y2==y) break;
-			if(x2==x) j=1;
-			else if(y2==y) j=0;
-			else {
-				if(Q[0][x2].first>Q[1][y2].first) j=0;
-				if(Q[0][x2].first<Q[1][y2].first) j=1;
-				if(Q[0][x2].first==Q[1][y2].first) {
-					if(Q[0][x2].second<Q[1][y2].second) j=0;
-					else j=1;
-				}
-			}
-			if(j==0) V.push_back(Q[0][x2++]);
-			else V.push_back(Q[1][y2++]);
-		}
-		
+		sort(ALL(V2));
+		reverse(ALL(V2));
+		ll pre=W+1;
+		FORR(vv,V2) if(vv.second<pre) V.push_back(vv), pre=vv.second;
 	}
-	cout<<V[0].first<<endl;
+	
+	ll ma=0;
+	FORR(v,V) ma=max(ma,v.first);
+	
+	
+	cout<<ma<<endl;
 	
 	
 }
