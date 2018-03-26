739668f308c48e50c9a037e187807931785f18db
kmjp <kmjp@users.noreply.github.com>
2018-03-27 00:53:37 +0900

    Fix yukicoder No.640.

--- yuki/601-700/640.cpp
+++ yuki/601-700/640.cpp
@@ -17,45 +17,54 @@ string S[75];
 int LR[76][76];
 int UD[76][76];
 
-void solve() {
-	int i,j,k,l,r,x,y; string s;
-	
-	cin>>N;
-	FOR(y,N) {
-		cin>>S[y];
-		FOR(x,N) {
-			LR[y+1][x+1]=LR[y+1][x]+(S[y][x]=='#');
-			UD[y+1][x+1]=UD[y][x+1]+(S[y][x]=='#');
-		}
-	}
-	
-	
+int hoge() {
 	int ma=0;
-	// all yoko
 	int ret=0;
+	int y,x;
+	FOR(y,N) FOR(x,N) {
+		LR[y+1][x+1]=LR[y+1][x]+(S[y][x]=='#');
+		UD[y+1][x+1]=UD[y][x+1]+(S[y][x]=='#');
+	}
+
 	FOR(y,N) if(LR[y+1][N-1]-LR[y+1][1]==0 && (S[y][0]=='.'||S[y][N-1]=='.')) ret++;
 	ma=max(ma,ret);
 	ret=0;
 	FOR(x,N) if(UD[N-1][x+1]-UD[1][x+1]==0 && (S[0][x]=='.'||S[N-1][x]=='.')) ret++;
 	ma=max(ma,ret);
+	return ma;
+}
+
+int hoge2(int L,int U,int R,int D) {
+	int x,y;
+	for(y=U;y<=D;y++) for(x=L;x<=R;x++) if(S[y][x]=='#') return -1;
+	for(y=U;y<=D;y++) for(x=L;x<=R;x++) S[y][x]='#';
+	int ret=1+hoge();
+	for(y=U;y<=D;y++) for(x=L;x<=R;x++) S[y][x]='.';
+	return ret;
+}
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
 	
-	int L=0,R=0,U=0,D=0;
-	if(UD[N-1][1]-UD[1][1]==0 && (S[0][0]=='.'||S[N-1][0]=='.')) L=1;
-	if(UD[N-1][N]-UD[1][N]==0 && (S[0][N-1]=='.'||S[N-1][N-1]=='.')) R=1;
-	FOR(y,N) {
-		if(LR[y+1][N]-LR[y+1][1]==0) L++;
-		if(LR[y+1][N-1]-LR[y+1][0]==0) R++;
-	}
-	if(LR[1][N-1]-LR[1][1]==0 && (S[0][0]=='.'||S[0][N-1]=='.')) U=1;
-	if(LR[N][N-1]-LR[N][1]==0 && (S[N-1][0]=='.'||S[N-1][N-1]=='.')) D=1;
-	FOR(x,N) {
-		if(UD[N][x+1]-UD[1][x+1]==0) U++;
-		if(UD[N-1][x+1]-UD[0][x+1]==0) D++;
-	}
+	cin>>N;
+	FOR(y,N) cin>>S[y];
 	
-	ma=max({ma,L,R,U,D});
 	
+	
+	int ma=hoge();
+	// rot
 	if(LR[1][N]==0 && LR[N][N]==0 && UD[N][1]==0 && UD[N][N]==0) ma=max(ma,4);
+	
+	ma=max(ma,hoge2(0,0,0,N-2));
+	ma=max(ma,hoge2(0,1,0,N-1));
+	ma=max(ma,hoge2(N-1,0,N-1,N-2));
+	ma=max(ma,hoge2(N-1,1,N-1,N-1));
+	ma=max(ma,hoge2(0,0,N-2,0));
+	ma=max(ma,hoge2(1,0,N-1,0));
+	ma=max(ma,hoge2(0,N-1,N-2,N-1));
+	ma=max(ma,hoge2(1,N-1,N-1,N-1));
+	
 	
 	cout<<ma<<endl;
 	
