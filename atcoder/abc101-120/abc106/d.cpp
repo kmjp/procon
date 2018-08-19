dc446d16855b2fe1b5d3625a76fdbe1f9ccf3875
kmjp <kmjp@users.noreply.github.com>
2018-08-19 20:45:51 +0900

    Add ABC#106 A/B/C/D.

---
+++ atcoder/abc101-120/abc106/d.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N,M,Q;;
+int dp[501][501];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M>>Q;
+	FOR(i,M) {
+		cin>>x>>y;
+		dp[x][y]++;
+	}
+	for(i=2;i<=N;i++) {
+		for(x=1;x+i-1<=N;x++) {
+			dp[x][x+i-1]+=dp[x+1][x+i-1]+dp[x][x+i-2]-dp[x+1][x+i-2];
+		}
+	}
+	FOR(i,Q) {
+		cin>>x>>y;
+		cout<<dp[x][y]<<endl;
+	}
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
