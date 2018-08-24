7788e5af884733d37943279e1c0fb5e7394f6083
kmjp <kmjp@users.noreply.github.com>
2018-08-24 23:33:37 +0900

    Add yukicoder No.725/726/727/728.

---
+++ yuki/701-800/728.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+template<class V, int ME> class BIT {
+public:
+	V bit[1<<ME];
+	V operator()(int e) {if(e<0) return 0;V s=0;e++;while(e) s+=bit[e-1],e-=e&-e; return s;}
+	void add(int e,V v) { e++; while(e<=1<<ME) bit[e-1]+=v,e+=e&-e;}
+};
+BIT<int,20> bt;
+
+int N;
+int A[301010];
+int L[303030];
+int R[303030];
+vector<pair<int,int>> E;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) cin>>A[i];
+	FOR(i,N) {
+		cin>>L[i]>>R[i];
+		E.push_back({A[i],i});
+		E.push_back({A[i]+R[i],N+i});
+	}
+	ll ret=0;
+	sort(ALL(E));
+	FORR(e,E) {
+		x=e.second%N;
+		if(e.second<N) {
+			x=e.second;
+			y=lower_bound(A,A+N,A[x]-L[x])-A;
+			ret+=bt(x)-bt(y-1);
+			bt.add(x,1);
+		}
+		else {
+			bt.add(x,-1);
+		}
+	}
+	cout<<ret<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
