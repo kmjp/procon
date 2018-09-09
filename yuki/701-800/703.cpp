fc74b400bf5d45bf19f72db3e8ec623907ef2464
kmjp <kmjp@users.noreply.github.com>
2018-09-09 12:03:04 +0900

    Fix yukicoder NO.703.

--- yuki/701-800/703.cpp
+++ yuki/701-800/703.cpp
@@ -12,48 +12,36 @@ typedef signed long long ll;
 #define MINUS(a) memset(a,0xff,sizeof(a))
 //-------------------------------------------------------
 
-template<typename V> struct ConvexHullOnline {
-	const V inf=1LL<<40+1;
-	const int cmptype=1; // 0-max 1-min
-	map<V,V> Q,PR; // Q[a]=b -> a*x+b, P[a]=c ax+b is best unti x<=c PR[c]=a
-	unordered_map<V,V> P;
-	ConvexHullOnline() { Q[inf]=-inf*inf; Q[-inf]=inf*inf;}
-	int ok(pair<V,V> A,pair<V,V> B, pair<V,V> C) { // max or min
-		if(A.first==-inf && C.first==inf) return 1;
-		return ((__int128_t)(B.second-A.second)*(B.first-C.first)<=(__int128)(C.second-B.second)*(A.first-B.first));
+
+template<typename V> struct ConvexHull {
+	deque<pair<V,V>> Q;
+	int cmptype=1; // 0-max 1-min
+	V calc(pair<V,V> p, V x) {
+		return p.first*x+p.second;
 	}
-	bool bad(typename map<V,V>::iterator it) {
-		if(it==Q.begin() || next(it)==Q.end() || next(it)->first==inf || prev(it)->first==-inf) return false;
-		return ok(*prev(it),*it,*next(it))==0;
+	int dodo(pair<V,V> A,pair<V,V> B, pair<V,V> C) { // max or min
+		return ((B.second-C.second)*(B.first-A.first)>=(A.second-B.second)*(C.first-B.first));
 	}
-	void erase(typename map<V,V>::iterator it) { PR.erase(P[it->first]); P.erase(it->first); Q.erase(it);}
-	void update(typename map<V,V>::iterator it) {
-		if(next(it)==Q.end() || it==Q.begin()) return;
-		auto it2=next(it);
-		if(it2->first==inf) {
-			P[it->first]=inf; PR[inf]=it->first;
-		}
-		else {
-			P[it->first]=(it2->second-it->second)/(it->first-it2->first);
-			PR[P[it->first]]=it->first;
-		}
+	void add(V a, V b) { // add ax+b
+		Q.push_back({a,b});
+		int v;
+		while((v=Q.size())>=3 && dodo(Q[v-3],Q[v-2],Q[v-1]))
+			Q[v-2]=Q[v-1], Q.pop_back();
+	}
+	void add(vector<pair<V,V>> v) {
+		sort(v.begin(),v.end());
+		if(cmptype==1) reverse(v.begin(),v.end());
+		for(auto r=v.begin();r!=v.end();r++) add(r->first,r->second);
 	}
 	
-	void add(V a, V b) { // add ax+b
-		if(cmptype) a=-a, b=-b;
-		if(Q.count(a)) if(Q[a]==b || Q[a]>b) return;
-		Q[a]=b;
-		auto it=Q.find(a);
-		if(bad(it)) return (void)Q.erase(it);
-		while(bad(next(it))) erase(next(it));
-		while(bad(prev(it))) erase(prev(it));
-		PR.erase(P[it->first]); P.erase(it->first);
-		if(prev(it)!=Q.begin()) PR.erase(P[prev(it)->first]); P.erase(prev(it)->first);
-		update(it);update(prev(it));
-	}
+	
 	V query(V x) {
-		auto it=PR.lower_bound(x);
-		return (it->second*x+Q[it->second])*(cmptype?-1:1);
+		int L=-1,R=Q.size()-1;
+		while(R-L>1) {
+			int M=(L+R)/2;
+			(((calc(Q[M],x)>=calc(Q[M+1],x)))?L:R)=M;
+		}
+		return calc(Q[R],x);
 	}
 };
 
@@ -62,7 +50,7 @@ ll A[303030];
 ll X[303030];
 ll Y[303030];
 ll D[303030];
-ConvexHullOnline<ll> cht;
+ConvexHull<ll> cht;
 
 void solve() {
 	int i,j,k,l,r,x,y; string s;
@@ -73,8 +61,8 @@ void solve() {
 	FOR(i,N) cin>>Y[i];
 	
 	FOR(i,N) {
-		cht.add(-2*X[i],D[i]+X[i]*X[i]+Y[i]*Y[i]);
-		D[i+1]=cht.query(A[i])+A[i]*A[i];
+		cht.add(2*X[i],D[i]+X[i]*X[i]+Y[i]*Y[i]);
+		D[i+1]=cht.query(-A[i])+A[i]*A[i];
 	}
 	cout<<D[N]<<endl;
 	
