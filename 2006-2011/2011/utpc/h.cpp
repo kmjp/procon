5807b46bc07e475bcffd79ede839e430a0e53d12
kmjp <kmjp@users.noreply.github.com>
2020-04-18 01:59:19 +0900

    Add UTPC 2011 H.

---
+++ 2006-2011/2011/utpc/h.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+
+template<int NV,class V> class MinCostFlow {
+public:
+	struct edge { int to; V capacity; V cost; int reve;};
+	vector<edge> E[NV]; int prev_v[NV], prev_e[NV]; V dist[NV];
+	void add_edge(int x,int y, V cap, V cost) {
+		E[x].push_back((edge){y,cap,cost,(int)E[y].size()});
+		E[y].push_back((edge){x,0, -cost,(int)E[x].size()-1}); /* rev edge */
+	}
+	
+	V mincost(int from, int to, ll flow) {
+		V res=0; int i,v;
+		ZERO(prev_v); ZERO(prev_e);
+		while(flow>0) {
+			fill(dist, dist+NV, numeric_limits<V>::max()/2);
+			dist[from]=0;
+			priority_queue<pair<V,int> > Q;
+			Q.push(make_pair(0,from));
+			while(Q.size()) {
+				V d=-Q.top().first;
+				int cur=Q.top().second;
+				Q.pop();
+				if(dist[cur]!=d) continue;
+				if(d==numeric_limits<V>::max()/2) break;
+				FOR(i,E[cur].size()) {
+					edge &e=E[cur][i];
+					if(e.capacity>0 && dist[e.to]>d+e.cost) {
+						dist[e.to]=d+e.cost;
+						prev_v[e.to]=cur;
+						prev_e[e.to]=i;
+						Q.push(make_pair(-dist[e.to],e.to));
+					}
+				}
+			}
+			
+			if(dist[to]==numeric_limits<V>::max()/2) return -1;
+			V lc=flow;
+			for(v=to;v!=from;v=prev_v[v]) lc = min(lc, E[prev_v[v]][prev_e[v]].capacity);
+			flow -= lc;
+			res += lc*dist[to];
+			for(v=to;v!=from;v=prev_v[v]) {
+				edge &e=E[prev_v[v]][prev_e[v]];
+				e.capacity -= lc;
+				E[v][e.reve].capacity += lc;
+			}
+		}
+		return res;
+	}
+};
+
+MinCostFlow<23050,ll> mcf;
+
+int M,N,K;
+int W[101010];
+int pre[101010];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	MINUS(pre);
+	cin>>M>>N>>K;
+	FOR(i,N) cin>>W[i];
+	FOR(i,K) mcf.add_edge(i,i+1,101010,10000);
+	ll sum=0;
+	int p=-1;
+	FOR(i,K) {
+		cin>>x;
+		x--;
+		if(p==x) {
+			i--;
+			K--;
+			continue;
+		}
+		p=x;
+		sum+=W[x];
+		if(pre[x]!=-1) {
+			mcf.add_edge(pre[x]+1,i,1,10000*(i-(pre[x]+1))-W[x]);
+		}
+		pre[x]=i;
+	}
+	
+	cout<<sum-(10000LL*(K-1)*(M-1)-mcf.mincost(0,(K-1),M-1))<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
