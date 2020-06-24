ee48a95317b7887f2768633aed5a8eb680bf9332
kmjp <kmjp@users.noreply.github.com>
2020-06-25 01:41:59 +0900

    Add Tenka 1 2013 Qual B C.

--- 2012-2016/2013/tenka/b/c.cpp
+++ 2012-2016/2013/tenka/b/c.cpp
@@ -1,141 +1,83 @@
-#include <cstdlib>
-#include <cstring>
-#include <memory>
-#include <cstdio>
-#include <fstream>
-#include <iostream>
-#include <cmath>
-#include <string>
-#include <sstream>
-#include <stack>
-#include <queue>
-#include <vector>
-#include <set>
-#include <map>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <unistd.h>
-
 typedef signed long long ll;
-typedef unsigned long long u64;
 
-#define _PE(...) printf(__VA_ARGS__); fprintf(stderr, __VA_ARGS__);
-#define _E(...) fprintf(stderr, __VA_ARGS__)
 #undef _P
-#define _P(...) printf(__VA_ARGS__)
-
-#define FOR(x,to) for(x=0;x<to;x++)
-#define FOR2(x,from,to) for(x=from;x<to;x++)
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
 #define ZERO(a) memset(a,0,sizeof(a))
-void _fill_int(int* p,int val,int rep) {int i;	FOR(i,rep) p[i]=val;}
-#define FILL_INT(a,val) _fill_int((int*)a,val,sizeof(a)/4)
-#define ZERO2(a,b) memset(a,0,b)
-#define MINUS(a) _fill_int((int*)a,-1,sizeof(a)/4)
-#define GETs(x) scanf("%s",x);
-int GETi() { int i;scanf("%d",&i); return i;}
-#define GET1(x) scanf("%d",x);
-#define GET2(x,y) scanf("%d%d",x,y);
-#define GET3(x,y,z) scanf("%d%d%d",x,y,z);
-
-#define EPS (1e-11)
-template <class T> T sqr(T val){ return val*val;}
-
+#define MINUS(a) memset(a,0xff,sizeof(a))
 //-------------------------------------------------------
 
-int W,H,A,B;
-int tate[8][8];
-int yoko[8][8];
-int ma=0;
+int H,W,A,B;
 
-int check() {
-	int num=0;
-	int x,y,a=0,b=0;
-	
-	for(y=1;y<H-1;y++) {
-		for(x=1;x<W-1;x++) {
-			if(tate[y-1][x]==0 && tate[y][x]==1 && yoko[y][x-1]==0 && yoko[y][x]==1) b++; //deko
-			if(tate[y-1][x]==1 && tate[y][x]==0 && yoko[y][x-1]==1 && yoko[y][x]==0) a++; //boko
-		}
-	}
-	
-	if(a<A || b>B) return 0;
-	
-	for(x=1;x<W-1;x++) {
-		if(yoko[0][x-1]==1 && yoko[0][x]==0 && tate[0][x]==0) num++;
-		if(yoko[H-1][x-1]==1 && yoko[H-1][x]==0 && tate[H-2][x]==1) num++;
-	}
-	for(y=1;y<H-1;y++) {
-		if(tate[y-1][0]==1 && tate[y][0]==0 && yoko[y][0]==0) num++;
-		if(tate[y-1][W-1]==1 && tate[y][W-1]==0 && yoko[y][W-2]==1) num++;
-	}
-	
-	return num;
-}
+int from[1<<8][19][19][2];
+int to[1<<8][19][19][2];
 
-void dfs(int y,int x) {
-	if(x>=W) x=0,y++;
-	
-	// null ok?
-	if(y<H-1 && x<W-1) {
-		tate[y][x]=0;yoko[y][x]=0;
-		dfs(y,x+1);
-		tate[y][x]=0;yoko[y][x]=1;
-		dfs(y,x+1);
-		tate[y][x]=1;yoko[y][x]=0;
-		dfs(y,x+1);
-		tate[y][x]=1;yoko[y][x]=1;
-		dfs(y,x+1);
-	}
-	else if(y==H-1 && x<W-1) {
-		yoko[y][x]=0;
-		dfs(y,x+1);
-		yoko[y][x]=1;
-		dfs(y,x+1);
-	}
-	else if(y<H-1 && x==W-1) {
-		tate[y][x]=0;
-		dfs(y,x+1);
-		tate[y][x]=1;
-		dfs(y,x+1);
-	}
-	else {
-		ma=max(ma,check());
-	}
-	
-}
+template<class T> void chmax(T &a, const T &b) { a=max(a,b);}
+
 
 void solve() {
-	int f,r,i,j,k,l,x,y,tx,ty,aa[5];
-	ma=0;
-	cin>>W>>H>>A>>B;
-	if(W<H) swap(W,H);
+	int i,j,k,l,r,x,y; string s;
 	
-	if(A==0 && B==(W-2)*(H-2)) {
-		//small1
-		if(B>=2) return (void)_P("%d\n",(W-2+1)/2*2+(H-2+1)/2*2);
-		if(B==1) return (void)_P("4\n"); //3x3
-		if(B==0) return (void)_P("%d\n",W-2); //
+	cin>>H>>W>>A>>B;
+	int mask,mask2;
+	
+	FOR(mask2,1<<W) FOR(x,19) FOR(y,19) from[mask2][x][y][0]=from[mask2][x][y][1]=-100;
+	
+	FOR(mask,1<<(W-1)) FOR(mask2,1<<W) {
+		int num=0;
+		for(x=1;x<W-1;x++) {
+			if((mask2&(1<<x))) continue;
+			if((mask&(1<<(x-1)))==0) continue;
+			if((mask&(1<<(x)))) continue;
+			num++;
+		}
+		chmax(from[mask2][0][0][0],num);
 	}
-	else if(H<=4) {
-		ZERO(tate);
-		ZERO(yoko);
-		dfs(0,0);
-		_P("%d\n",ma);
+	int a,b;
+	for(y=1;y<H;y++) FOR(x,W) {
+		FOR(mask,1<<W) FOR(a,19) FOR(b,19) to[mask][a][b][0]=to[mask][a][b][1]=-100;
+		FOR(mask,1<<W) FOR(a,19) FOR(b,19) {
+			i=(mask>>x)&1;
+			FOR(j,2) FOR(k,2) FOR(l,2) {
+				int cur=from[mask][a][b][l];
+				if(cur<0) continue;
+				
+				int nmask=(mask&(255-(1<<x)))|(k<<x);
+				if(x==0) {
+					if(l==1) continue;
+					chmax(to[nmask][a][b][j],cur+(y<H-1&&i==1&&j==0&&k==0));
+				}
+				else if(x==W-1) {
+					if(j==1) continue;
+					chmax(to[nmask][a][b][j],cur+(y<H-1&&i==1&&l==1&&k==0));
+				}
+				else {
+					if(y==H-1&&k) continue;
+					int a2=a+((i==1)&&(j==0)&&(k==0)&&(l==1)&&(y!=H-1));
+					int b2=b+((i==0)&&(j==1)&&(k==1)&&(l==0)&&(y!=H-1));
+					int add=i==1&&j==0&&l==1&&y==H-1;
+					if(a2<=18 && b2<=18) chmax(to[nmask][a2][b2][j],cur+add);
+				}
+			}
+		}
+		swap(from,to);
 	}
 	
-	return;
+	int ma=0;
+	FOR(x,19) FOR(y,19) if(x>=A && y<=B) ma=max(ma,from[0][x][y][0]);
+	
+	cout<<ma<<endl;
 }
 
 
 int main(int argc,char** argv){
-	
-	if(argc>1) freopen(argv[1], "r", stdin);
-	solve();
-	
-	return 0;
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
 }
