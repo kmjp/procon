517922dbbd6e83d745444ed67d27e19701af6866
kmjp <kmjp@users.noreply.github.com>
2021-03-14 00:15:34 +0900

    Add ABC#195 A/B/C/D/E/F.

---
+++ atcoder/abc151-200/abc195/f.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+ll A,B;
+
+const int prime_max = 72;
+vector<int> prime;
+int NP,divp[prime_max];
+
+void cprime() {
+	if(NP) return;
+	for(int i=2;i<prime_max;i++) if(divp[i]==0) {
+		//M[i]=NP;
+		prime.push_back(i); NP++;
+		for(ll j=1LL*i*i;j>=i&&j<prime_max;j+=i) if(divp[j]==0) divp[j]=i;
+	}
+}
+
+ll dp[1<<20];
+
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>A>>B;
+	dp[0]=1;
+	cprime();
+	
+	for(ll a=A;a<=B;a++) {
+		int nmask=0;
+		FOR(j,NP) if(a%prime[j]==0) nmask|=1<<j;
+		for(int mask=(1<<20)-1;mask>=0;mask--) {
+			if((mask&nmask)==0) dp[mask|nmask]+=dp[mask];
+		}
+	}
+	
+	ll ret=0;
+	FOR(i,1<<20) ret+=dp[i];
+	
+	cout<<ret<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
