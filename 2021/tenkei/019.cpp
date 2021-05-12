35069467233195d921e7f511fa68ca3959d8365c
kmjp <kmjp@users.noreply.github.com>
2021-05-13 00:54:54 +0900

    Add tenkei 90 No.016/017/018/019/020.

---
+++ 2021/tenkei/019.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N;
+int A[402];
+ll dp[402][402];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,2*N) cin>>A[i];
+	FOR(i,2*N-1) dp[i][i+2]=abs(A[i]-A[i+1]);
+	
+	for(int len=4;len<=2*N;len+=2) {
+		for(x=0;x+len<=2*N;x++) {
+			dp[x][x+len]=dp[x+1][x+len-1]+abs(A[x]-A[x+len-1]);
+			for(y=2;y<len;y+=2) dp[x][x+len]=min(dp[x][x+len],dp[x][x+y]+dp[x+y][x+len]);
+		}
+	}
+	cout<<dp[0][2*N]<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
