37bcf7ea5cbb53c0e2edd68fc069832d337b4376
kmjp <kmjp@users.noreply.github.com>
2021-05-15 01:13:36 +0900

    Add Tenkei 90 No.026/027/028/029/030.

---
+++ 2021/tenkei/029.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+
+//区間最大値更新・区間最大値取得
+static ll const def=0;
+template<class V,int NV> class SegTree_3 {
+public:
+	vector<V> val,ma; //区間全体を覆う最大値・区間内のいずれかの最大値
+	SegTree_3(){
+		val.resize(NV*2,def);
+		ma.resize(NV*2,def);
+	};
+	
+	V getval(int x,int y,int l=0,int r=NV,int k=1) {
+		if(r<=x || y<=l) return def;
+		if(x<=l && r<=y) return ma[k];
+		return max({val[k],getval(x,y,l,(l+r)/2,k*2),getval(x,y,(l+r)/2,r,k*2+1)});
+	}
+	
+	void update(int x,int y, V v,int l=0,int r=NV,int k=1) {
+		if(l>=r) return;
+		if(x<=l && r<=y) {
+			val[k]=max(val[k],v);
+			ma[k]=max(ma[k],v);
+		}
+		else if(l < y && x < r) {
+			update(x,y,v,l,(l+r)/2,k*2);
+			update(x,y,v,(l+r)/2,r,k*2+1);
+			ma[k]=max({ma[k],ma[k*2],ma[k*2+1]});
+		}
+	}
+};
+
+SegTree_3<int,1<<20> st;
+int W,N;
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>W>>N;
+	FOR(i,N) {
+		int L,R;
+		cin>>L>>R;
+		L--;
+		x=st.getval(L,R)+1;
+		cout<<x<<endl;
+		st.update(L,R,x);
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
