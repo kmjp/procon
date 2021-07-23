fbaed0db412b75fc88de15f6ad69b69f395837a6
kmjp <kmjp@users.noreply.github.com>
2021-07-24 01:07:07 +0900

    Add ABC#205 A/B/C/D/E/F.

---
+++ atcoder/abc201-250/abc205/d.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,Q;
+ll A[101010];
+ll K;
+
+ll hoge(ll v) {
+	int x=lower_bound(A,A+N,v+1)-A;
+	return v-x;
+}
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>Q;
+	FOR(i,N) cin>>A[i];
+	sort(A,A+N);
+	while(Q--) {
+		cin>>K;
+		ll a=1LL<<60;
+		for(i=59;i>=0;i--) if(hoge(a-(1LL<<i))>=K) a-=1LL<<i;
+		cout<<a<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
---
+++ atcoder/abc201-250/abc205/f.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+
+int H,W,N;
+int A,B,C,D;
+
+template<class V> class MaxFlow_dinic {
+public:
+	struct edge { int to,reve;V cap;};
+	static const int MV = 12000;
+	vector<edge> E[MV];
+	int itr[MV],lev[MV];
+	void add_edge(int x,int y,V cap,bool undir=false) {
+		E[x].push_back((edge){y,(int)E[y].size(),cap});
+		E[y].push_back((edge){x,(int)E[x].size()-1,undir?cap:0});
+	}
+	void bfs(int cur) {
+		MINUS(lev);
+		queue<int> q;
+		lev[cur]=0;
+		q.push(cur);
+		while(q.size()) {
+			int v=q.front(); q.pop();
+			FORR(e,E[v]) if(e.cap>0 && lev[e.to]<0) lev[e.to]=lev[v]+1, q.push(e.to);
+		}
+	}
+	V dfs(int from,int to,V cf) {
+		if(from==to) return cf;
+		for(;itr[from]<E[from].size();itr[from]++) {
+			edge* e=&E[from][itr[from]];
+			if(e->cap>0 && lev[from]<lev[e->to]) {
+				V f=dfs(e->to,to,min(cf,e->cap));
+				if(f>0) {
+					e->cap-=f;
+					E[e->to][e->reve].cap += f;
+					return f;
+				}
+			}
+		}
+		return 0;
+	}
+	V maxflow(int from, int to) {
+		V fl=0,tf;
+		while(1) {
+			bfs(from);
+			if(lev[to]<0) return fl;
+			ZERO(itr);
+			while((tf=dfs(from,to,numeric_limits<V>::max()))>0) fl+=tf;
+		}
+	}
+};
+MaxFlow_dinic<int> mf;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>H>>W>>N;
+	FOR(y,H) mf.add_edge(10000,10100+y,1);
+	FOR(x,W) mf.add_edge(10200+x,10300,1);
+	FOR(i,N) {
+		cin>>A>>B>>C>>D;
+		A--,C--,B--,D--;
+		mf.add_edge(i*2,i*2+1,1);
+		for(y=A;y<=C;y++) mf.add_edge(10100+y,i*2,1);
+		for(x=B;x<=D;x++) mf.add_edge(i*2+1,10200+x,1);
+	}
+	cout<<mf.maxflow(10000,10300)<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
