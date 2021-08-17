100d811bcd95629ee094a64e965d054c2b1c63ea
kmjp <kmjp@users.noreply.github.com>
2021-08-18 00:14:08 +0900

    Add yukicoder No.1616/1617/1618/1619/1620/1621.

---
+++ yuki/1601-1700/1619.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+ll N,M,K;
+
+template<class V, int ME> class BIT {
+public:
+	V bit[1<<ME],val[1<<ME];
+	V operator()(int e) {if(e<0) return 0;V s=0;e++;while(e) s+=bit[e-1],e-=e&-e; return s;}
+	void add(int e,V v) { val[e++]+=v; while(e<=1<<ME) bit[e-1]+=v,e+=e&-e;}
+	void set(int e,V v) { add(e,v-val[e]);}
+	int lower_bound(V val) {
+		V tv=0; int i,ent=0;
+		for(i=ME-1;i>=0;i--) if(tv+bit[ent+(1<<i)-1]<val) tv+=bit[ent+(1<<i)-1],ent+=(1<<i);
+		return ent;
+	}
+};
+BIT<int,20> bit;
+
+ll P[101010];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M>>K;
+	M-=N*(N-1)/2;
+	FOR(i,N) bit.add(i,1);
+	FOR(i,N) {
+		x=min(K,(ll)N-1-i);
+		y=bit.lower_bound(x+1);
+		P[i]=y;
+		bit.add(y,-1);
+		if(y==N-1) P[i]+=M;
+		K-=x;
+		
+		cout<<P[i]<<endl;
+		
+		
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
