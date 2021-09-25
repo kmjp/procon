8f1659b64189dc03e836e79fcd55e44364dd3ea2
kmjp <kmjp@users.noreply.github.com>
2021-09-26 01:17:17 +0900

    Fix yukicoder No.1640.

--- yuki/1601-1700/1640.cpp
+++ yuki/1601-1700/1640.cpp
@@ -13,79 +13,71 @@ template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;
 template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
 //-------------------------------------------------------
 
-template<class V> class MaxFlow_dinic {
-public:
-	struct edge { int to,reve;V cap;};
-	static const int MV = 202020;
-	vector<edge> E[MV];
-	int itr[MV],lev[MV];
-	void add_edge(int x,int y,V cap,bool undir=false) {
-		E[x].push_back((edge){y,(int)E[y].size(),cap});
-		E[y].push_back((edge){x,(int)E[x].size()-1,undir?cap:0});
-	}
-	void bfs(int cur) {
-		MINUS(lev);
-		queue<int> q;
-		lev[cur]=0;
-		q.push(cur);
-		while(q.size()) {
-			int v=q.front(); q.pop();
-			FORR(e,E[v]) if(e.cap>0 && lev[e.to]<0) lev[e.to]=lev[v]+1, q.push(e.to);
-		}
-	}
-	V dfs(int from,int to,V cf) {
-		if(from==to) return cf;
-		for(;itr[from]<E[from].size();itr[from]++) {
-			edge* e=&E[from][itr[from]];
-			if(e->cap>0 && lev[from]<lev[e->to]) {
-				V f=dfs(e->to,to,min(cf,e->cap));
-				if(f>0) {
-					e->cap-=f;
-					E[e->to][e->reve].cap += f;
-					return f;
-				}
-			}
-		}
-		return 0;
-	}
-	V maxflow(int from, int to) {
-		V fl=0,tf;
-		while(1) {
-			bfs(from);
-			if(lev[to]<0) return fl;
-			ZERO(itr);
-			while((tf=dfs(from,to,numeric_limits<V>::max()))>0) fl+=tf;
-		}
+template<int um> class UF {
+	public:
+	vector<int> par,rank,cnt;
+	UF() {par=rank=vector<int>(um,0); cnt=vector<int>(um,1); for(int i=0;i<um;i++) par[i]=i;}
+	void reinit(int num=um) {int i; FOR(i,num) rank[i]=0,cnt[i]=1,par[i]=i;}
+	int operator[](int x) {return (par[x]==x)?(x):(par[x] = operator[](par[x]));}
+	int count(int x) { return cnt[operator[](x)];}
+	int operator()(int x,int y) {
+		if((x=operator[](x))==(y=operator[](y))) return x;
+		cnt[y]=cnt[x]=cnt[x]+cnt[y];
+		if(rank[x]>rank[y]) return par[x]=y;
+		rank[x]+=rank[x]==rank[y]; return par[y]=x;
 	}
 };
+UF<101010> uf;
 
-MaxFlow_dinic<int> mf;
 int N;
 int A[101010],B[101010];
+vector<pair<int,int>> E[101010];
+vector<int> lp;
+int C[101010];
+int ret[101010];
 
+void dfs(int cur,int pre) {
+	FORR2(e,i,E[cur]) {
+		if(e==pre) {
+			ret[i]=cur;
+		}
+		else {
+			dfs(e,cur);
+		}
+	}
+}
 
 void solve() {
 	int i,j,k,l,r,x,y; string s;
 	
 	cin>>N;
 	FOR(i,N) {
-		mf.add_edge(200000,i,1);
-		mf.add_edge(100000+i,200001,1);
 		cin>>A[i]>>B[i];
-		A[i]--;
-		B[i]--;
-		mf.add_edge(i,100000+A[i],1);
-		mf.add_edge(i,100000+B[i],1);
-	}
-	if(mf.maxflow(200000,200001)!=N) {
-		cout<<"No"<<endl;
-	}
-	else {
-		cout<<"Yes"<<endl;
-		FOR(i,N) {
-			FORR(e,mf.E[i]) if(e.to>=100000&&e.cap==0) cout<<(e.to-100000+1)<<endl;
+		A[i]--,B[i]--;
+		
+		if(uf[A[i]]==uf[B[i]]) {
+			lp.push_back(i);
+		}
+		else {
+			uf(A[i],B[i]);
+			E[A[i]].push_back({B[i],i});
+			E[B[i]].push_back({A[i],i});
 		}
 	}
+	FOR(i,N) {
+		C[uf[A[i]]]++;
+		C[uf[B[i]]]++;
+	}
+	FOR(i,N) if(uf[i]==i && C[i]!=uf.count(i)*2) return _P("No\n");
+	
+	FORR(c,lp) {
+		ret[c]=A[c];
+		dfs(A[c],A[c]);
+	}
+	cout<<"Yes"<<endl;
+	FOR(i,N) cout<<ret[i]+1<<endl;
+	
+	
 	
 }
 
