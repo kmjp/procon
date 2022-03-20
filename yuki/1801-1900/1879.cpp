6424ceb697fc2467ed934e3a3a840fa67ca541d5
kmjp <kmjp@users.noreply.github.com>
2022-03-21 00:02:27 +0900

    Add yukicoder No.1877/1878/1879/1880.

---
+++ yuki/1801-1900/1879.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+ll N;
+const ll mo=1000000007;
+
+const int MAT=8;
+struct Mat { ll v[MAT][MAT]; Mat(){ZERO(v);};};
+
+Mat mulmat(Mat& a,Mat& b,int n=MAT) {
+	ll mo2=4*mo*mo;
+	int x,y,z; Mat r;
+	FOR(x,n) FOR(y,n) r.v[x][y]=0;
+	FOR(x,n) FOR(z,n) FOR(y,n) {
+		r.v[x][y] += a.v[x][z]*b.v[z][y];
+		if(r.v[x][y]>mo2) r.v[x][y] -= mo2;
+	}
+	FOR(x,n) FOR(y,n) r.v[x][y]%=mo;
+	return r;
+}
+
+Mat powmat(ll p,Mat a,int n=MAT) {
+	int i,x,y; Mat r;
+	FOR(x,n) FOR(y,n) r.v[x][y]=0;
+	FOR(i,n) r.v[i][i]=1;
+	while(p) {
+		if(p%2) r=mulmat(r,a,n);
+		a=mulmat(a,a,n);
+		p>>=1;
+	}
+	return r;
+}
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	Mat A;
+	A.v[1][0]=A.v[2][1]=A.v[3][2]=1;
+	A.v[5][4]=A.v[6][5]=A.v[7][6]=1;
+	A.v[4][0]=1;
+	A.v[0][1]=1;
+	A.v[4][2]=1;
+	A.v[0][3]=1;
+	A.v[4][5]=1;
+	A.v[4][7]=1;
+	A=powmat(N,A);
+	cout<<A.v[(N%2)*4][0]<<endl;
+		
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
