b1f6742cc627ebc7206f04405bc2968ced0a57cf
kmjp <kmjp@users.noreply.github.com>
2022-03-25 01:39:46 +0900

    Add ABC#244 A/B/C/D/E/F/G/H.

---
+++ atcoder/abc201-250/abc244/d.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+
+string S[3],T[3];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>S[0]>>S[1]>>S[2];
+	cin>>T[0]>>T[1]>>T[2];
+	
+	x=y=0;
+	if(S[0]<S[1]) x^=1;
+	if(S[0]<S[2]) x^=1;
+	if(S[1]<S[2]) x^=1;
+	if(T[0]<T[1]) x^=1;
+	if(T[0]<T[2]) x^=1;
+	if(T[1]<T[2]) x^=1;
+	if(x) {
+		cout<<"No"<<endl;
+	}
+	else {
+		cout<<"Yes"<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
