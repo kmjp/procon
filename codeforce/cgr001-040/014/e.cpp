853b1896973cddf4991d0e185451b68199c17b32
kmjp <kmjp@users.noreply.github.com>
2022-09-27 00:31:19 +0900

    Add CGR#014 A/B/C/D/E/F/G.

---
+++ codeforce/cgr001-040/014/e.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N;
+ll mo;
+
+ll from[404];
+ll to[404];
+
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	cin>>N>>mo;
+	
+	from[1]=1;
+	ll ret=0;
+	for(i=1;i<=N;i++) {
+		ZERO(to);
+		FOR(x,N+1) {
+			if(i+(x-1)==N) {
+				ret+=from[x];
+				continue;
+			}
+			if(i+(x-1)>N) {
+				continue;
+			}
+			// add new
+			(to[x+1]+=from[x]*(x+1))%=mo;
+			// con
+			(to[x]+=from[x]*(2*x))%=mo;
+			
+		}
+		swap(from,to);
+	}
+	
+	
+	
+	cout<<ret%mo<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
