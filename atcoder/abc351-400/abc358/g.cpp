8daee187f3f9643fc7f999b2b43660c093d4162e
kmjp <kmjp@users.noreply.github.com>
2024-06-18 00:07:52 +0900

    Add ABC#358 A/B/C/D/E/F/G.

---
+++ atcoder/abc351-400/abc358/g.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int H,W;
+int K;
+int SY,SX;
+int A[55][55];
+ll dp[2600][55][55];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>H>>W>>K;
+	cin>>SY>>SX;
+	SY--,SX--;
+	FOR(y,H) FOR(x,W) {
+		cin>>A[y][x];
+		dp[0][y][x]=-1LL<<60;
+	}
+	dp[0][SY][SX]=0;
+	
+	FOR(i,2500) {
+		FOR(y,H) FOR(x,W) {
+			dp[i+1][y][x]=-1LL<<60;
+			chmax(dp[i+1][y][x],dp[i][y][x]+A[y][x]);
+			if(y) chmax(dp[i+1][y][x],dp[i][y-1][x]+A[y][x]);
+			if(x) chmax(dp[i+1][y][x],dp[i][y][x-1]+A[y][x]);
+			if(y+1<H) chmax(dp[i+1][y][x],dp[i][y+1][x]+A[y][x]);
+			if(x+1<W) chmax(dp[i+1][y][x],dp[i][y][x+1]+A[y][x]);
+		}
+	}
+	
+	ll ret=0;
+	FOR(y,H) FOR(x,W) {
+		if(K<=2500) {
+			ret=max(ret,dp[K][y][x]);
+		}
+		else {
+			ret=max(ret,dp[2500][y][x]+1LL*(K-2500)*A[y][x]);
+		}
+	}
+	cout<<ret<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
