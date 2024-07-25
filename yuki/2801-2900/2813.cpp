bc9dcd82d8fc95729d229f7ac684bd51cf710765
kmjp <kmjp@users.noreply.github.com>
2024-07-26 00:23:03 +0900

    Add yukicoder No.2811/2812/2813/2814/2815/2816/2817/2818.

---
+++ yuki/2801-2900/2813.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int T,N,A[202020];
+
+ll G(ll v) {
+	if(v<=2) return 1;
+	if(v<=4) return 2;
+	if(v%3==1) return (v-4)/3*4+2;
+	return (v-2)/3*4;
+}
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>T;
+	while(T--) {
+		cin>>N;
+		y=0;
+		FOR(i,N) {
+			cin>>x;
+			y^=G(x);
+		}
+		if(y==0) {
+			cout<<"Bob"<<endl;
+		}
+		else {
+			cout<<"Alice"<<endl;
+		}
+	}
+		
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
---
+++ yuki/2801-2900/2816.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int T;
+ll N;
+const ll mo=998244353;
+
+ll modpow(ll a, ll n = mo-2) {
+	ll r=1;a%=mo;
+	while(n) r=r*((n%2)?a:1)%mo,a=a*a%mo,n>>=1;
+	return r;
+}
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>T;
+	while(T--) {
+		cin>>N;
+		ll ret=0;
+		//頂点1
+		ret=modpow(2,1LL*N*(N-1)/2);
+		if(N>1) {
+			//直通
+			(ret+=(N-1)*modpow(2,1LL*N*(N-1)/2-1))%=mo;
+			if(N>2) {
+				//2手で行けるケース
+				ll a=(modpow(4,(N-2))-modpow(3,(N-2))+mo);
+				(a*=N-1)%=mo;
+				(a*=modpow(2,1LL*N*(N-1)/2-1-2*(N-2)))%=mo;
+				ret+=a;
+			}
+		}
+		cout<<ret%mo<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
