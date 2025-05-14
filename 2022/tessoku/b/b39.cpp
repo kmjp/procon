7c519da5d73ba4ce14e8df0ff62d6e9a57578aa8
kmjp <kmjp@users.noreply.github.com>
2025-05-15 04:18:25 +0900

    Add Tessoku 2022 B 36/37/38/39.

---
+++ 2022/tessoku/b/b39.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,D;
+vector<int> add[2020];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>D;
+	FOR(i,N) {
+		cin>>x>>y;
+		add[x].push_back(y);
+	}
+	multiset<int> M;
+	ll ret=0;
+	FOR(i,D+1) {
+		FORR(a,add[i]) M.insert(a);
+		if(M.size()) {
+			ret+=*M.rbegin();
+			M.erase(--M.end());
+		}
+	}
+	cout<<ret<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
