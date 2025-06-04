5de9891a0903a8ed61b5cdab749609f75f8a0827
kmjp <kmjp@users.noreply.github.com>
2025-06-05 00:20:20 +0900

    Add ABC#408 A/B/C/D/E/F/G.

---
+++ atcoder/abc401-450/abc408/e.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M;
+vector<pair<int,int>> E[202020];
+int vis[202020];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M;
+	FOR(i,M) {
+		cin>>x>>y>>k;
+		E[x-1].push_back({y-1,k});
+		E[y-1].push_back({x-1,k});
+	}
+	
+	int ret=(1<<30)-1;
+	for(i=29;i>=0;i--) {
+		int mask=ret^(1<<i);
+		ZERO(vis);
+		vis[0]=1;
+		queue<int> Q;
+		Q.push(0);
+		while(Q.size()) {
+			x=Q.front();
+			Q.pop();
+			FORR2(e,c,E[x]) if(vis[e]==0&&(c|mask)==mask) {
+				vis[e]=1;
+				Q.push(e);
+			}
+		}
+		
+		
+		if(vis[N-1]) {
+			ret=mask;
+		}
+	}
+	cout<<ret<<endl;
+	
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
