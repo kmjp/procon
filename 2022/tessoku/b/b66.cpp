99f7d1410d26af4cbf6c7cf3822817ccc4bb38fe
kmjp <kmjp@users.noreply.github.com>
2025-07-09 00:03:43 +0900

    Add Tessoku 2022 B64/65/66.

---
+++ 2022/tessoku/b/b66.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M,Q;
+int A[101010],B[101010];
+int D[101010];
+int T[101010],X[101010],Y[101010];
+int ret[101010];
+
+template<int um> class UF {
+	public:
+	vector<int> par,rank,cnt;
+	UF() {par=rank=vector<int>(um,0); cnt=vector<int>(um,1); for(int i=0;i<um;i++) par[i]=i;}
+	void reinit(int num=um) {int i; FOR(i,num) rank[i]=0,cnt[i]=1,par[i]=i;}
+	int operator[](int x) {return (par[x]==x)?(x):(par[x] = operator[](par[x]));}
+	int count(int x) { return cnt[operator[](x)];}
+	int operator()(int x,int y) {
+		if((x=operator[](x))==(y=operator[](y))) return x;
+		cnt[y]=cnt[x]=cnt[x]+cnt[y];
+		if(rank[x]>rank[y]) return par[x]=y;
+		rank[x]+=rank[x]==rank[y]; return par[y]=x;
+	}
+};
+UF<101010> uf;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M;
+	FOR(i,M) {
+		cin>>A[i]>>B[i];
+	}
+	cin>>Q;
+	FOR(i,Q) {
+		cin>>T[i]>>X[i];
+		if(T[i]==1) {
+			X[i]--;
+			D[X[i]]=1;
+		}
+		else {
+			cin>>Y[i];
+		}
+	}
+	FOR(i,M) if(D[i]==0) uf(A[i],B[i]);
+	for(i=Q-1;i>=0;i--) {
+		if(T[i]==1) {
+			uf(A[X[i]],B[X[i]]);
+		}
+		else {
+			ret[i]=uf[X[i]]==uf[Y[i]];
+		}
+	}
+	FOR(i,Q) if(T[i]==2) {
+		if(ret[i]) cout<<"Yes"<<endl;
+		else cout<<"No"<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
