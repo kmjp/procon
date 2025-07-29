7746b2fe722391efad74c76f004210a968a27382
kmjp <kmjp@users.noreply.github.com>
2025-07-30 04:19:06 +0900

    Add yukicodee\r No.3209/3210/3211/3212.

---
+++ yuki/3201-3300/3211.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+int Q,K;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>Q>>K;
+	if(K>=5) {
+		cout<<"Yes"<<endl;
+		FOR(i,Q) {
+			if(i==0) cout<<"1 2"<<endl;
+			else if(i==1) cout<<"2 3"<<endl;
+			else if(i==2) cout<<"1 4"<<endl;
+			else if(i==3) cout<<"1 5"<<endl;
+			else if(i==4) cout<<"1 5"<<endl;
+			else cout<<"6 7"<<endl;
+		}
+	}
+	else if(K==2) {
+		if(Q==1) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+		}
+		else {
+			cout<<"No"<<endl;
+		}
+	}
+	else if(K==3) {
+		if(Q==1) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+		}
+		else if(Q==2) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+		}
+		else if(Q==3) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"3 4"<<endl;
+		}
+		else {
+			cout<<"No"<<endl;
+		}
+	}
+	else {
+		if(Q==1) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+		}
+		else if(Q==2) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+		}
+		else if(Q==3) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"3 4"<<endl;
+		}
+		else if(Q==4) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"3 4"<<endl;
+			cout<<"3 4"<<endl;
+		}
+		else if(Q==5) {
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"3 4"<<endl;
+			cout<<"3 4"<<endl;
+		}
+		else {
+			cout<<"No"<<endl;
+		}
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
