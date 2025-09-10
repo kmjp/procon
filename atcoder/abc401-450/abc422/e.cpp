1145ea026e96c039f40dcbd84519476df3c30d6d
kmjp <kmjp@users.noreply.github.com>
2025-09-11 04:58:28 +0900

    Add ABC#422 E/F/G.

---
+++ atcoder/abc401-450/abc422/e.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N;
+
+ll X[505050],Y[505050];
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) cin>>X[i]>>Y[i];
+	
+	FOR(i,50) {
+		x=rand()%N;
+		y=rand()%N;
+		if(x==y) x=(x+1)%N;
+		int num=0;
+		FOR(j,N) {
+			if(j==x) continue;
+			if(j==y) continue;
+			if((Y[y]-Y[x])*(X[j]-X[x])-(Y[j]-Y[x])*(X[y]-X[x])==0) num++;
+		}
+		if(num+2>N/2) {
+			int dy=Y[y]-Y[x];
+			int dx=X[y]-X[x];
+			ll a=-dy;
+			ll b=dx;
+			ll c=-a*X[x]-b*Y[x];
+			cout<<"Yes"<<endl;
+			cout<<a<<" "<<b<<" "<<c<<endl;
+			return;
+			
+		}
+		
+	}
+	cout<<"No"<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
---
+++ atcoder/abc401-450/abc422/g.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,A,B,C;
+
+const int mo=998244353;
+ll modpow(ll a, ll n = mo-2) {
+	ll r=1; a%=mo;
+	while(n) r=r*((n%2)?a:1)%mo,a=a*a%mo,n>>=1;
+	return r;
+}
+
+template <class T> using vec=vector<T>; //using vec=valarray<T>;
+
+template<class T> vec<T> fft(vec<T> v, bool rev=false) {
+	int n=v.size(),i,j,m;
+	for(int m=n; m>=2; m/=2) {
+		T wn=modpow(5,(mo-1)/m);
+		if(rev) wn=modpow(wn);
+		for(i=0;i<n;i+=m) {
+			T w=1;
+			for(int j1=i,j2=i+m/2;j2<i+m;j1++,j2++) {
+				T t1=v[j1],t2=v[j2];
+				v[j1]=(t1+t2+mo)%mo;
+				v[j2]=ll(t1+mo-t2)*w%mo;
+				while(v[j1]>=mo) v[j1]-=mo;
+				w=(ll)w*wn%mo;
+			}
+		}
+	}
+	for(i=0,j=1;j<n-1;j++) {
+		for(int k=n>>1;k>(i^=k);k>>=1);
+		if(i>j) swap(v[i],v[j]);
+	}
+	if(rev) {
+		ll rv = modpow(n);
+		FOR(i,n) v[i]=(ll)v[i]*rv%mo;
+	}
+	return v;
+}
+
+template<class T> vec<T> MultPoly(vec<T> P,vec<T> Q,bool resize=false,bool recover=false) {
+	int len=0;
+	if(resize) {
+		int maxind=0,pi=-1,qi=-1,i;
+		int s=2;
+		len=P.size()+Q.size()-1;
+		FOR(i,P.size()) if(norm(P[i])) pi=i;
+		FOR(i,Q.size()) if(norm(Q[i])) qi=i;
+		if(pi==-1||qi==-1) return {};
+		maxind=pi+qi+1;
+		while(s*2<maxind) s*=2;
+		
+		if(s<=64) { //fastpath
+			vec<T> R(s*2);
+			for(int x=0;x<=pi;x++) for(int y=0;y<=qi;y++) (R[x+y]+=P[x]*Q[y])%=mo;
+			if(recover) R.resize(len);
+			return R;
+		}
+		vec<T> P2(s*2),Q2(s*2);
+		FOR(i,pi+1) P2[i]=P[i];
+		FOR(i,qi+1) Q2[i]=Q[i];
+		swap(P,P2),swap(Q,Q2);
+	}
+	P=fft(P), Q=fft(Q);
+	for(int i=0;i<P.size();i++) P[i]=(ll)P[i]*Q[i]%mo;
+	P=fft(P,true);
+	if(resize&&recover) P.resize(len);
+	return P;
+}
+
+const int NUM_=400001;
+static ll fact[NUM_+1],factr[NUM_+1],inv[NUM_+1];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	inv[1]=fact[0]=factr[0]=1;
+	for (int i=2;i<=NUM_;++i) inv[i] = inv[mo % i] * (mo - mo / i) % mo;
+	for (int i=1;i<=NUM_;++i) fact[i]=fact[i-1]*i%mo, factr[i]=factr[i-1]*inv[i]%mo;
+	
+	cin>>N>>A>>B>>C;
+	vector<ll> F(N+1),G(N+1),H(N+1);
+	
+	FOR(j,2) {
+		FOR(i,N+1) {
+			if(i%A==0) F[i]=(j==0?1:factr[i]);
+			if(i%B==0) G[i]=(j==0?1:factr[i]);
+			if(i%C==0) H[i]=(j==0?1:factr[i]);
+		}
+		auto FGH=MultPoly(F,MultPoly(G,H,1),1);
+		FGH.resize(N+1);
+		
+		cout<<FGH[N]*(j==0?1:fact[N])%mo<<endl;
+	}
+		
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
