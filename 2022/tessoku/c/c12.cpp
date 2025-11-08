c96cfcb1a85519ce7b0f933c7df7672cde6838b7
kmjp <kmjp@users.noreply.github.com>
2025-11-09 00:33:52 +0900

    Add Tessoku 2022 C10/11/12/13.

---
+++ 2022/tessoku/c/c12.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M,K;
+int S[300][300];
+int dp[300][10];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M>>K;
+	FOR(i,M) {
+		cin>>x>>y;
+		for(j=1;j<=x;j++) for(k=y;k<=N;k++) S[j][k]++;
+	}
+	
+	MINUS(dp);
+	dp[0][0]=0;
+	FOR(i,K) {
+		FOR(j,N) if(dp[j][i]>=0) for(x=j+1;x<=N;x++) chmax(dp[x][i+1],dp[j][i]+S[j+1][x]);
+	}
+	cout<<dp[N][K]<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
