dde23fa3903fb61717da07169869fdfcd4797507
kmjp <kmjp@users.noreply.github.com>
2025-12-14 00:23:34 +0900

    Add ARC#211 A/B/C.

---
+++ atcoder/arc201-250/arc211/a.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int T;
+ll A[10];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>T;
+	while(T--) {
+		ll S=0;
+		FOR(i,9) {
+			cin>>A[i+1];
+			S+=A[i+1];
+		}
+		ll ma=0;
+		for(i=1;i<=4;i++) {
+			if(A[i]+A[10-i]==S&&A[i]&&A[10-i]) ma=1;
+		}
+		ll a=A[5];
+		ll b=S-A[5];
+		ma=max(ma,a-b-1);
+		
+		
+		cout<<ma<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
