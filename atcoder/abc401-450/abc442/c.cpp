4d56a9f951c9c8cd838e45d1e1309f07c325abf6
kmjp <kmjp@users.noreply.github.com>
2026-02-01 00:10:35 +0900

    Add ABC#442 A/B/C/D/E.

---
+++ atcoder/abc401-450/abc442/c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M;
+ll C[202020];
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M;
+	FOR(i,N) C[i]=N-1;
+	FOR(i,M) {
+		cin>>x>>y;
+		C[x-1]--;
+		C[y-1]--;
+	}
+	FOR(i,N) {
+		cout<<C[i]*(C[i]-1)*(C[i]-2)/6<<" ";
+	}
+	cout<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
