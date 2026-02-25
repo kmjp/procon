ce1521377e43ccee65b2599642de4f3c8544293a
kmjp <kmjp@users.noreply.github.com>
2026-02-26 00:43:31 +0900

    Add AWC#011 A/B/C/D/E.

---
+++ atcoder/awc001-050/awc011/e.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M;
+ll A[1010],B[1010];
+ll L[1010][3030];
+ll R[1010][3030];
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M;
+	FOR(i,N) {
+		cin>>A[i+1]>>B[i+1];
+		FOR(j,M+1) {
+			L[i+1][j]=L[i][j];
+			if(j) L[i+1][j]=max(L[i+1][j],L[i+1][j-1]);
+			if(j>=A[i+1]) L[i+1][j]=max(L[i+1][j],L[i][j-A[i+1]]+B[i+1]);
+		}
+	}
+	for(i=N;i>=1;i--) {
+		FOR(j,M+1) {
+			R[i][j]=R[i+1][j];
+			if(j) R[i][j]=max(R[i][j],R[i][j-1]);
+			if(j>=A[i]) R[i][j]=max(R[i][j],R[i+1][j-A[i]]+B[i]);
+		}
+	}
+	ll ma=L[N][M];
+	for(i=1;i<=N;i++) {
+		if(A[i]>M) {
+			cout<<"No"<<endl;
+			continue;
+		}
+		int lef=M-A[i];
+		int ok=0;
+		FOR(x,lef+1) if(L[i-1][x]+B[i]+R[i+1][lef-x]==ma) ok=1;
+		if(ok) {
+			cout<<"Yes"<<endl;
+		}
+		else {
+			cout<<"No"<<endl;
+		}
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
