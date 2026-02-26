65aed24150285fbce9a8fc11d23d9c2462c74d0b
kmjp <kmjp@users.noreply.github.com>
2026-02-27 00:34:46 +0900

    Add AWC#012 A/B/C/D/E.

---
+++ atcoder/awc001-050/awc012/e.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,K;
+ll A[202020];
+
+ll const def=-1LL<<60;;
+template<class V,int NV> class SegTree_ma {
+public:
+	vector<V> val;
+	
+	V comp(V l,V r){ return max(l,r);};
+	
+	SegTree_ma(){val=vector<V>(NV*2,def);};
+	V getval(int x,int y,int l=0,int r=NV,int k=1) { // x<=i<y
+		if(r<=x || y<=l) return def;
+		if(x<=l && r<=y) return val[k];
+		return comp(getval(x,y,l,(l+r)/2,k*2),getval(x,y,(l+r)/2,r,k*2+1));
+	}
+	void update(int entry, V v) {
+		entry += NV;
+		val[entry]=v;
+		while(entry>1) entry>>=1, val[entry]=comp(val[entry*2],val[entry*2+1]);
+	}
+};
+SegTree_ma<ll,1<<20> st;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>K;
+	FOR(i,N) {
+		cin>>A[i];
+		if(i==0) {
+			st.update(0,A[0]);
+		}
+		else {
+			ll v=A[i]+st.getval(max(0,i-K),i);
+			st.update(i,v);
+		}
+	}
+	cout<<st.getval(N-1,N)<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
