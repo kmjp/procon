4d03c8e42fb8570c80062f1150c5ec5f2a374174
kmjp <kmjp@users.noreply.github.com>
2026-03-17 01:13:48 +0900

    Add ECR#180 E/F.

---
+++ codeforce/edu/161-200/180/f.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M;
+ll E[555][555];
+ll D[555][555];
+
+ll K;
+int Q;
+ll A[555],ME[555],MD[555],op[555];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N>>M;
+	FOR(x,N) FOR(y,N) E[x][y]=(x==y)?0:1LL<<40;
+	FOR(i,M) {
+		cin>>x>>y>>k;
+		E[y-1][x-1]=min(E[y-1][x-1],(ll)k);
+	}
+	FOR(x,N) FOR(y,N) D[x][y]=E[x][y];
+	FOR(k,N) FOR(x,N) FOR(y,N) D[x][y]=min(D[x][y],D[x][k]+D[k][y]);
+	
+	cin>>Q;
+	while(Q--) {
+		cin>>K;
+		FOR(i,N) {
+			cin>>A[i];
+			ME[i]=MD[i]=A[i];
+			op[i]=1LL<<40;
+		}
+		FOR(x,N) FOR(y,N) {
+			//Å’Z˜H
+			MD[y]=min(MD[y],A[x]+D[x][y]);
+			//Å’Z‚Å‚Í‚È‚¢‚©‚à
+			ME[y]=min(ME[y],A[x]+E[x][y]);
+		}
+		
+		// Å’Z˜H‚É‚·‚é‚½‚ß‚É•K—v‚È‘€ì‰ñ”
+		FOR(x,N) FOR(y,N) if(D[x][y]<E[x][y]) op[y]=min(op[y],A[x]+D[x][y]-(ME[y]-1));
+		FOR(x,N) cout<<(op[x]<=K);
+		cout<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
