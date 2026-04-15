8221034d6c5697c4a1be1c0926aea613f12121f3
kmjp <kmjp@users.noreply.github.com>
2026-04-16 00:28:58 +0900

    Add ABC#446 F/G.

---
+++ atcoder/abc401-450/abc446/g.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N;
+int A[505050];
+vector<int> P[505050];
+ll dp[505050];
+ll dpS[505050];
+const ll mo=998244353;
+
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) {
+		cin>>A[i];
+		P[A[i]].push_back(i);
+	}
+	dp[0]=dpS[0]=1;
+	ll ret=0;
+	FOR(i,N) {
+		int v=A[i];
+		x=lower_bound(ALL(P[v]),i)-P[v].begin();
+		ll nv=dpS[i];
+		if(x) {
+			(nv+=mo-dpS[P[v][x-1]+1])%=mo;
+		}
+		y=x+v-1;
+		if(y<P[v].size()) {
+			ret+=nv;
+			(dp[P[v][y]+1]+=nv)%=mo;
+		}
+		dpS[i+1]=(dpS[i]+dp[i+1])%mo;
+	}
+	cout<<ret%mo<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
