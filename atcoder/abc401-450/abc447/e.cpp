2366ffc143dcf03db570445e3b89579916f02ae0
kmjp <kmjp@users.noreply.github.com>
2026-04-17 00:11:26 +0900

    Add ABC#447 A/B/C/D/E.

---
+++ atcoder/abc401-450/abc447/e.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int N,M;
+int U[202020],V[202020];
+const ll mo=998244353;
+
+template<int um> class UF {
+	public:
+	vector<int> par,rank,cnt,G[um];
+	UF() {par=rank=vector<int>(um,0); cnt=vector<int>(um,1); for(int i=0;i<um;i++) par[i]=i;}
+	void reinit(int num=um) {int i; FOR(i,num) rank[i]=0,cnt[i]=1,par[i]=i;}
+	int operator[](int x) {return (par[x]==x)?(x):(par[x] = operator[](par[x]));}
+	int count(int x) { return cnt[operator[](x)];}
+	int operator()(int x,int y) {
+		if((x=operator[](x))==(y=operator[](y))) return x;
+		cnt[y]=cnt[x]=cnt[x]+cnt[y];
+		if(rank[x]>rank[y]) return par[x]=y;
+		rank[x]+=rank[x]==rank[y]; return par[y]=x;
+	}
+};
+UF<202020> uf;
+ll p2[202020];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	p2[0]=1;
+	FOR(i,201010) p2[i+1]=2*p2[i]%mo;
+	cin>>N>>M;
+	int num=N;
+	ll ret=0;
+	FOR(i,M) cin>>U[i]>>V[i];
+	for(i=M-1;i>=0;i--) {
+		if(uf[U[i]]!=uf[V[i]]) {
+			if(num>2) {
+				num--;
+				uf(U[i],V[i]);
+			}
+			else {
+				(ret+=p2[i+1])%=mo;
+			}
+		}
+		
+	}
+	cout<<ret<<endl;
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
