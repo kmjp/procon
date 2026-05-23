cf3f8a7c3ac40b9168640969941971962f69d64a
kmjp <kmjp@users.noreply.github.com>
2026-05-24 02:26:23 +0900

    Add AWC#036 A/B/C/D/E.

---
+++ atcoder/awc001-050/awc036/e.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int H,W;
+ll A[19][19];
+ll S[19][1<<19];
+
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	int mask;
+	cin>>H>>W;
+	FOR(y,H) {
+		FOR(x,W) cin>>A[y][x];
+		FOR(mask,1<<W) {
+			FOR(x,W) if(mask&(1<<x)) S[y][mask]+=A[y][x];
+		}
+	}
+	ll ma=-1LL<<60;
+	FOR(mask,1<<W) {
+		ll sum=0;
+		FOR(y,H) sum+=max(S[y][mask],S[y][(1<<W)-1]);
+		ma=max(ma,sum);
+	}
+	cout<<ma<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
