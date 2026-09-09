781f5d1534a3db7c6a43ef974d1c41702b5fd64c
kmjp <kmjp@users.noreply.github.com>
2026-09-10 00:29:31 +0900

    Add yukicoder No.3683/3684/3685.

---
+++ yuki/3601-3700/3685.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+template<class T> bool chmax(T &a, const T &b) { if(a<b){a=b;return 1;}return 0;}
+template<class T> bool chmin(T &a, const T &b) { if(a>b){a=b;return 1;}return 0;}
+//-------------------------------------------------------
+
+int T;
+string R,S;
+int K;
+
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>T;
+	while(T--) {
+		cin>>R>>S>>K;
+		int N=R.size();
+		if(S=="Warong") {
+			FOR(i,K) R[i]='A';
+			
+			if(count(ALL(R),'A')==N-1) {
+				FOR(i,N) if(R[i]!='A') R[i]='W';
+			}
+		}
+		else {
+			if(count(R.begin(),R.begin()+K,'A')==K) {
+				for(i=K;i<N;i++) R[i]='A';
+			}
+			else if(count(R.begin()+K,R.end(),'W')&&count(R.begin(),R.begin()+K,'A')==K-1) {
+				FOR(i,K) if(R[i]!='A') R[i]='W';
+			}
+		}
+		cout<<R<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
